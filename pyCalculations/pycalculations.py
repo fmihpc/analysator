0b2f6803160e490ce19f961d7798b170c419cf25
ohannuks <otto.hannuksela@fmi.fi>
2013-09-06 06:45:59 +0000

    Added a module which imports the necessary functions

---
+++ pyCalculations/pycalculations.py
@@ -0,0 +1,5 @@
+# List of functions and classes that should be imported into the interface
+from cutthrough import cut_through
+from fourier import fourier
+from variable import VariableInfo
+from virtualspacecraft import virtual_spacecraft
