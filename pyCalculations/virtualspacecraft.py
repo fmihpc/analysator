d245d442c86d3975ce8dd11cba74297517ed5c56
galfthan <sebastian.von.alfthan@fmi.fi>
2013-09-11 08:40:15 +0000

    Spacecraft now also adds time parameters, time timestep and fileindex

--- pyCalculations/virtualspacecraft.py
+++ pyCalculations/virtualspacecraft.py
@@ -18,12 +18,24 @@ def virtual_spacecraft( vlsvReader_list, variables, cellids, units="" ):
    vlsvReader_list = np.atleast_1d(vlsvReader_list)
    variables = np.atleast_1d(variables)
    cellids = np.atleast_1d(cellids)
-   data = [[] for i in xrange(len(cellids)*len(variables))]
+   parameters = ["t","tstep","fileIndex"]
+   parameter_units=["s","",""]
+   #construct empty units, if none are given
+   if (units == "") or (len(units) != len(variables)):
+      units=[ "" for i in xrange(len(variables))]
+   #construct data 
+   data = [[] for i in xrange(len(parameters)+len(cellids)*len(variables))]
    for t in xrange(len(vlsvReader_list)):
       # Get the vlsv reader
       vlsvReader = vlsvReader_list[t]
       # Open the vlsv reader's file:
       vlsvReader.optimize_open_file()
+      #go through parameters
+      for j in xrange(len(parameters)):
+         # Read the parameter
+         # Save the data into the right slot in the data array:
+         data[j].append(vlsvReader.read_parameter( parameters[j]))
+
       # Go through variables:
       for j in xrange(len(variables)):
          variable = variables[j]
@@ -31,16 +43,15 @@ def virtual_spacecraft( vlsvReader_list, variables, cellids, units="" ):
          #variables_for_cellids = vlsvReader.read_variables_for_cellids( variable, cellids )
          # Save the data into the right slot in the data array:
          for i in xrange(len(cellids)):
-            data[i*len(variables)+j].append(vlsvReader.read_variable( variable, cellids[i] ))
+            data[len(parameters)+i*len(variables)+j].append(vlsvReader.read_variable( variable, cellids[i] ))
       # For optimization purposes we are now freeing vlsvReader's memory
       # Note: Upon reading data vlsvReader created an internal hash map that takes a lot of memory
       vlsvReader.optimize_clear_fileindex_for_cellid()
       # Close the vlsv reader's file:
       vlsvReader.optimize_close_file()
    from output import output_1d
-   if (units == "") or (len(units) != len(variables)):
-      return output_1d( data, [variables[(int)(i)%(int)(len(variables))] for i in xrange(len(data))] )
-   else:
-      return output_1d( data, [variables[(int)(i)%(int)(len(variables))] for i in xrange(len(data))], [units[(int)(i)%(int)(len(units))] for i in xrange(len(data))] )
+   return output_1d( data, 
+                     parameters +  [variables[(int)(i)%(int)(len(variables))] for i in xrange(len(data)-len(parameters))], 
+                     parameter_units + [units[(int)(i)%(int)(len(units))] for i in xrange(len(data)-len(parameters))] )
 
 
