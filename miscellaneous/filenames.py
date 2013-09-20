5d0721d932575b8206f1c261c897bbfae658333b
ohannuks <otto.hannuksela@fmi.fi>
2013-09-20 07:28:34 +0000

    Added documentation

--- miscellaneous/filenames.py
+++ miscellaneous/filenames.py
@@ -1,7 +1,14 @@
 import glob
 import os
 
+'''This module imports glob and os and has some miscellaneous functions for basic file manipulation
+'''
+
 def get_sorted_file_names(name="*.vlsv"):
+   '''Gets the file names in the current directory and sorts them.
+      :param name            Name of the file(s), for example "*.vlsv"
+      returns a list of file names in sorted order
+   '''
    fileNames=glob.glob(name)
    fileNames.sort()
    return fileNames
