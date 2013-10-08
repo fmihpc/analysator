9be20482e629b1e13580b0ae7b26c2707457bbda
ohannuks <otto.hannuksela@fmi.fi>
2013-10-08 13:25:12 +0000

    Added data reduction

---
+++ pyVlsv/reducer.py
@@ -0,0 +1,24 @@
+class DataReducerVariable:
+   ''' A class for creating custom variables that are being read by the VlsvFile class. This is useful for reading in variables that are not written in the vlsv file directly
+   '''
+   variables = []
+   operation = []
+   name = []
+   units = []
+   def __init__(self, variables, operation, name, units):
+      ''' Constructor for the class
+          :param variables          List of variables for doing calculations with
+          :param operation          The operator that operates on the variables
+          :param name               Name of the new variable
+          :param units              Units of the variable
+          Example:
+          def plus( array ):
+             return array[0]+array[1]
+          new_var = DataReducerVariable( ["rho", "rho"], plus, "sum_of_rhos", "1/m^3" )
+      '''
+      self.variables = variables
+      self.operation = operation
+      self.name = name
+      self.units = units
+
+
