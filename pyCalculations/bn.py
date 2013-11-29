2c20531b97783d279dca62b3c6f9f3f4e97ee744
ohannuks <otto.hannuksela@fmi.fi>
2013-11-29 15:11:18 +0000

    Fixed bn

--- pyCalculations/bn.py
+++ pyCalculations/bn.py
@@ -25,7 +25,10 @@ def B_n_bowshock( vlsvReader, points ):
           print B_n_vectors
 
        .. note:: B_n vector will be of length points-1
+
+       .. note:: THIS IS ONLY IN THE X-Y PLANE! THIS WILL NOT WORK WITH SIMULATIONS THAT HAVE FOR EX. Y-Z PLANE
    '''
+   from cutthrough import cut_through
    if len(points) < 2:
       print "Invalid point length, points array must be more than 1"
       return
@@ -36,17 +39,34 @@ def B_n_bowshock( vlsvReader, points ):
    vlsvReader.optimize_open_file()
 
    for i in xrange(len(points)-1):
+      # Get point 1 and point 2
       point1 = np.array(points[i], copy=False)
       point2 = np.array(points[i+1], copy=False)
-      # Calculate the normal and position of the middle of the line:
-      position = point1 + 0.5*(point2 - point1)
+
+      # Calculate the normal unit vector
       n_unit = np.dot(rotation_matrix_2d( 0.5*np.pi ), (point2 - point1)) / np.linalg.norm(point2 - point1)
-      # Get the value of B:
-      B_position = position + n_unit * 20000
-      # Read B:
-      cellid = vlsvReader.get_cellid( B_position )
-      B = vlsvReader.read_variable( name="B", operator="pass", cellids=cellid )
-      B_n_vectors.append( B.dot( n_unit ) )
+      n_unit = n_unit * np.array([1,1,0])
+
+      # Get the points shifted by unit vector times 3e6
+      point1_shifted = point1 + n_unit * 3.0e6
+      point2_shifted = point2 + n_unit * 3.0e6
+      
+     # get the cell ids
+      cutthrough = cut_through( vlsvReader, point1_shifted, point2_shifted )
+      cellids = cutthrough[0].data
+      print len(cellids)
+      # Read B values for cellids:
+      B_values = vlsvReader.read_variable( name="B", operator="pass", cellids=cellids )
+
+      # Go through each B (if more than one value):
+      if len(np.shape(B_values)) == 2:
+         for B in B_values:
+            B = B * np.array([1,1,0])
+            B_n_vectors.append( np.arccos( B.dot( n_unit ) / np.linalg.norm(B)) )
+      else:
+         # Only one B value:
+         B_values = B_values * np.array([1,1,0])
+         B_n_vectors.append( np.arccos( B_values.dot( n_unit ) / np.linalg.norm(B_values) ) )
 
    # Close file
    vlsvReader.optimize_close_file()
