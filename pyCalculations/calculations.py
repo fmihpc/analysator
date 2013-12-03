a1363b7661117ba1c8e0e9c1a28b008dd3826803
ohannuks <otto.hannuksela@fmi.fi>
2013-12-03 11:57:25 +0000

    Removed cellids file as it's included in the vlsvReader

--- pyCalculations/calculations.py
+++ pyCalculations/calculations.py
@@ -3,6 +3,5 @@ from cutthrough import cut_through
 from fourier import fourier
 from variable import VariableInfo
 from timeevolution import cell_time_evolution
-from cellids import cellids
 from pitchangle import pitch_angles
 import fit
