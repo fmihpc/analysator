4253d27e4e24e4800a6ce358d59d80a644304c09
ohannuks <otto.hannuksela@fmi.fi>
2014-02-13 15:20:09 +0200

    added documentation

--- pyMayaVi/mayavigrid.py
+++ pyMayaVi/mayavigrid.py
@@ -31,6 +31,36 @@ signal.signal(signal.SIGINT, SigHandler)
 class MayaviGrid(HasTraits):
    '''Class for constructing plots with MayaVi
    '''
+   ''' This class is used to plot the data in a vlsv file as a mayavi grid The following will bring up a new window and plot the grid in the vlsv file:
+
+   .. code-block:: python
+
+      grid = pt.grid.MayaviGrid(vlsvReader=f, variable="rho", operator='pass', threaded=False)
+
+   Once you have the window open you can use the picker tool in the right-upper corner and use various point-click tools for analyzing data.
+
+   Picker options:
+   
+   **None** Does nothing upon clicking somewhere in the grid
+   
+   **Velocity_space** Plots the velocity space at a specific position upon clicking somewhere in the grid Note: If the vlsv file does not have the velocity space at the position where you are clicking, this will not work
+   
+   **Velocity_space_iso_surface** Plots the velocity space at a specific position upon clicking somewhere in the grid in iso-surface plotting style Note: If the vlsv file does not have the velocity space at the position where you are clicking, this will not work
+   
+   **Velocity_space_nearest_cellid** Plots the velocity space of the closest cell id to the picking point Note: If the vlsv file does not have velocity space saved at all, this will not work
+   
+   **Velocity_space_nearest_cellid_iso_surface** Plots the velocity space of the closest cell id to the picking point in iso-surface plotting style Note: If the vlsv file does not have velocity space saved at all, this will not work
+   
+   **Pitch_angle** Plots the pitch angle distribution at the clicking position Note: If the vlsv file does not have the velocity space at the position where you are clicking, this will not work
+   
+   **Cut_through** Is used to plot or save the cut-through between two clicking points. This option requires you to use the args section at top-left. To use the args section to plot variables you must write for example: **plot rho B,x E,y** Upon clicking at two points a new window would open with a cut-through plot of rho, x-component of B and y-component of E Alternatively, you can save the cut-through to a variable in the MayaviGrid class by typing instead: **rho B,x E,y** and then going to the terminal and typing
+   
+   .. code-block:: python
+
+      cut_through_data = grid.cut_through
+      print cut_through_data
+
+   '''
    picker = Enum('None',
                  'Velocity_space',
                  "Velocity_space_nearest_cellid",
