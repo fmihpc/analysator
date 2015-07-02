659bee60c3351dd32655245adc3438131308a66c
Otto Hannuksela <otto.hannuksela@fmi.fi>
2015-07-02 17:18:50 +0300

    Removed visualization

--- miscellaneous/vtkwriter.py
+++ miscellaneous/vtkwriter.py
@@ -60,34 +60,34 @@ def write_vtk_file( filename, point_data ):
    polyDataWriter.Write()
    #######################################
    
-   # Read and visualize the written line
-   #######################################
-   reader = vtk.vtkPolyDataReader()
-   reader.SetFileName(filename)
-    
-   mapper = vtk.vtkPolyDataMapper()
-   if vtk.VTK_MAJOR_VERSION <= 5:
-       mapper.SetInput(reader.GetOutput())
-   else:
-       mapper.SetInputConnection(reader.GetOutputPort())
-    
-   actor = vtk.vtkActor()
-   actor.SetMapper(mapper)
-   
-   # Create a rendering window and renderer
-   ren = vtk.vtkRenderer()
-   renWin = vtk.vtkRenderWindow()
-   renWin.AddRenderer(ren)
-    
-   # Create a renderwindowinteractor
-   iren = vtk.vtkRenderWindowInteractor()
-   iren.SetRenderWindow(renWin)
-    
-   # Assign actor to the renderer
-   ren.AddActor(actor)
-    
-   # Enable user interface interactor
-   iren.Initialize()
-   renWin.Render()
-   iren.Start()
-   #######################################
+#   # Read and visualize the written line
+#   #######################################
+#   reader = vtk.vtkPolyDataReader()
+#   reader.SetFileName(filename)
+#    
+#   mapper = vtk.vtkPolyDataMapper()
+#   if vtk.VTK_MAJOR_VERSION <= 5:
+#       mapper.SetInput(reader.GetOutput())
+#   else:
+#       mapper.SetInputConnection(reader.GetOutputPort())
+#    
+#   actor = vtk.vtkActor()
+#   actor.SetMapper(mapper)
+#   
+#   # Create a rendering window and renderer
+#   ren = vtk.vtkRenderer()
+#   renWin = vtk.vtkRenderWindow()
+#   renWin.AddRenderer(ren)
+#    
+#   # Create a renderwindowinteractor
+#   iren = vtk.vtkRenderWindowInteractor()
+#   iren.SetRenderWindow(renWin)
+#    
+#   # Assign actor to the renderer
+#   ren.AddActor(actor)
+#    
+#   # Enable user interface interactor
+#   iren.Initialize()
+#   renWin.Render()
+#   iren.Start()
+#   #######################################
