d4587fe17c27aafeb0137b34339b8c6455e92fe3
Ilja <ilja.honkonen@fmi.fi>
2017-05-16 16:37:05 +0300

    Improve error messages when importing pytools submodules fails

--- pytools.py
+++ pytools.py
@@ -13,22 +13,22 @@ filemanagement.sys.path.insert(0, filemanagement.os.path.dirname(filemanagement.
 # Import modules
 try:
    import calculations
-except ImportError:
-   print "Note: Did not import calculations module"
+except ImportError as e:
+   print "Note: Did not import calculations module: ", e
 try:
    import vlsvfile
-except ImportError:
-   print "Note: Did not import vlsvfile module"
+except ImportError as e:
+   print "Note: Did not import vlsvfile module: ", e
 try:
    import grid
-except ImportError:
-   print "Note: Did not import grid module"
+except ImportError as e:
+   print "Note: Did not import grid module: ", e
 try:
    import plot
-except ImportError:
-   print "Note: Did not import plot module"
+except ImportError as e:
+   print "Note: Did not import plot module: ", e
 try:
    import miscellaneous
-except ImportError:
-   print "Note: Did not import miscellaneous"
+except ImportError as e:
+   print "Note: Did not import miscellaneous: ", e
 
