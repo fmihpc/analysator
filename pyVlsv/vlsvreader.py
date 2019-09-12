26cf305832d0ea27b601fa07bbb027a64778b5bb
Urs Ganse <urs.ganse@helsinki.fi>
2019-09-12 16:33:53 +0300

    Initial (kinda coarse) implementation of a fsgrid variable reader.
    This is still not working, but committed for testing on puhti.

--- pyVlsv/vlsvreader.py
+++ pyVlsv/vlsvreader.py
@@ -907,6 +907,90 @@ class VlsvReader(object):
          # Done.
          return ret_array
 
+   def read_fsgrid_variable(self, name, operator="pass"):
+       ''' Reads fsgrid variables from the open vlsv file.
+       Arguments:
+       :param name: Name of the variable
+       :param operator: Datareduction operator. "pass" does no operation on data
+       :returns: *ordered* numpy array with the data
+
+       ... seealso:: :func:`read_variable`
+       '''
+
+       # Get fsgrid domain size (this can differ from vlasov grid size if refined)
+       bbox = self.read(tag="MESH_BBOX", mesh="fsgrid")
+
+       # Read the raw array data
+       rawData = self.read(mesh='fsgrid', name=name, tag="VARIABLE", operator=operator)
+
+       # Determine fsgrid domain decomposition
+       numWritingRanks = self.read_parameter("numWritingRanks")
+       orderedData = np.array([bbox[0],bbox[1],bbox[2],rawData.shape[1]])
+
+       # Helper functions ported from c++ (fsgrid.hpp)
+       def computeDomainDecomposition(globalsize, ntasks):
+           processDomainDecomposition = [1,1,1]
+           processBox = [0,0,0]
+           optimValue = 999999999999999.
+           for i in range(1,min(ntasks,globalsize[0])):
+               processBox[0] = max(globalsize[0]/i,1)
+               for j in range(1,min(ntasks,globalsize[1])):
+                   if(i * j > ntasks):
+                       break
+                   processBox[1] = max(globalsize[1]/j,1)
+                   for k in range(1,min(ntasks,globalsize[2])):
+                       if(i * j * k > ntasks):
+                           break
+                       processBox[2] = max(globalsize[2]/k,1)
+                       value = 10 * processBox[0] * processBox[1] * processBox[2] + \
+                        (processBox[1] * processBox[2] if i>1 else 0) + \
+                        (processBox[0] * processBox[2] if j>1 else 0) + \
+                        (processBox[0] * processBox[1] if k>1 else 0)
+                       if(value < optimValue):
+                           optimeValue = value
+                           processDomainDecomposition=[i,j,k]
+           return processDomainDecomposition
+
+       def calcLocalStart(globalCells, ntasks, my_n):
+           n_per_task = globalCells/ntasks
+           remainder = globalCells%ntasks
+           if my_n < remainder:
+               return my_n * (n_per_task+1)
+           else:
+               return my_n * n_per_task + remainder
+       def calcLocalSize(globalCells, ntasks, my_n):
+           n_per_task = globalCells/ntasks
+           remainder = globalCells%ntasks
+           if my_n < remainder:
+               return n_per_task+1;
+           else:
+               return n_per_task
+
+
+       currentOffset = 0;
+       fsgridDecomposition = computeDomainDecomposition([bbox[0],bbox[1],bbox[2]],numWritingRanks)
+       for i in range(0,numWritingRanks):
+
+           thatTasksSize = [calcLocalSize(bbox[0], fsgridDecomposition[0], i), \
+                            calcLocalSize(bbox[1], fsgridDecomposition[2], i), \
+                            calcLocalSize(bbox[0], fsgridDecomposition[0], i)]
+           thatTasksStart = [calcLocalStart(bbox[0], fsgridDecomposition[0], i), \
+                             calcLocalStart(bbox[1], fsgridDecomposition[2], i), \
+                             calcLocalStart(bbox[0], fsgridDecomposition[0], i)]
+           thatTasksEnd = thatTasksStart + thatTasksSize
+
+           totalSize = thatTasksSize[0]*thatTasksSize[1]*thatTasksSize[2]
+
+           thatTasksData = rawData[currentOffset:currentOffset+totalSize,:]
+           thatTasksData.reshape([thatTasksSize[0],thatTasksSize[1],thatTasksSize[2],rawData.shape[1]])
+           orderedData[thatTasksStart[0]:thatTasksEnd[0],thatTasksStart[1]:thatTasksEnd[1],thatTasksStart[2]:thatTasksEnd[2],:] = thatTasksdata
+
+
+       if len(a.shape) == 1:
+           return a.reshape([bbox[0],bbox[1],bbox[2]])
+       else:
+           return a.reshape([bbox[0],bbox[1],bbox[2],a.shape[1]])
+
    def read_variable(self, name, cellids=-1,operator="pass"):
       ''' Read variables from the open vlsv file. 
       Arguments:
