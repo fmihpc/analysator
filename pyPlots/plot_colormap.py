more fixes to masking

pyPlots/plot_colormap.py
@@ -704,8 +704,8 @@ def plot_colormap(filename=None,
     rhomap = np.ma.masked_less_equal(np.ma.masked_invalid(rhomap), 0)
     rhomap = np.ma.masked_where(~np.isfinite(datamap), rhomap)
     XYmask = rhomap.mask
-    if np.ma.is_masked(XYmask):
-        if XYmask.mask.all():
+    if XYmask.any():
+        if XYmask.all():
             # if everything was masked in rhomap, allow plotting
             XYmask[:,:] = False
         else:
@@ -888,7 +888,7 @@ def plot_colormap(filename=None,
         if np.ma.is_masked(maskgrid):
             flux_function = flux_function[MaskX[0]:MaskX[-1]+1,:]
             flux_function = flux_function[:,MaskY[0]:MaskY[-1]+1]
-        if np.ma.is_masked(XYmask):
+        if XYmask.any():
             flux_function = np.ma.array(flux_function, mask=XYmask)
         # The flux level contours must be fixed instead of scaled based on min/max values in order
         # to properly account for flux freeze-in and advection with plasma
@@ -912,7 +912,7 @@ def plot_colormap(filename=None,
             if np.ma.is_masked(maskgrid):
                 fSmap = fSmap[MaskX[0]:MaskX[-1]+1,:]
                 fSmap = fSmap[:,MaskY[0]:MaskY[-1]+1]
-            if np.ma.is_masked(XYmask):
+            if XYmask.any():
                 fSmap = np.ma.array(fSmap, mask=XYmask)            
             fScont = ax1.contour(XmeshCentres,YmeshCentres,fSmap,[0.5],colors=fScolour, 
                                  linestyles='solid',linewidths=0.5,zorder=2)
@@ -936,7 +936,7 @@ def plot_colormap(filename=None,
         if np.ma.is_masked(maskgrid):
             vectmap = vectmap[MaskX[0]:MaskX[-1]+1,:,:]
             vectmap = vectmap[:,MaskY[0]:MaskY[-1]+1,:]
-        if np.ma.is_masked(XYmask):
+        if XYmask.any():
             vectmap = np.ma.array(vectmap)
             for i in range(3):
                 vectmap[:,:,i].mask = XYmask
@@ -979,7 +979,7 @@ def plot_colormap(filename=None,
         if np.ma.is_masked(maskgrid):
             slinemap = slinemap[MaskX[0]:MaskX[-1]+1,:,:]
             slinemap = slinemap[:,MaskY[0]:MaskY[-1]+1,:]
-        if np.ma.is_masked(XYmask):
+        if XYmask.any():
             slinemap = np.ma.array(slinemap)
             for i in range(3):
                 slinemap[:,:,i].mask = XYmask

pyPlots/plot_colormap3dslice.py
@@ -874,10 +874,14 @@ def plot_colormap3dslice(filename=None,
     # excluded by this method. Also mask away regions where datamap is invalid
     rhomap = np.ma.masked_less_equal(np.ma.masked_invalid(rhomap), 0)
     rhomap = np.ma.masked_where(~np.isfinite(datamap), rhomap)
-    if np.ma.is_masked(rhomap):
-        XYmask = rhomap.mask
-        # Mask datamap
-        datamap = np.ma.array(datamap, mask=XYmask)
+    XYmask = rhomap.mask
+    if XYmask.any():
+        if XYmask.all():
+            # if everything was masked in rhomap, allow plotting
+            XYmask[:,:] = False
+        else:
+            # Mask datamap
+            datamap = np.ma.array(datamap, mask=XYmask)
 
     #If automatic range finding is required, find min and max of array
     # Performs range-finding on a masked array to work even if array contains invalid values
@@ -1034,7 +1038,7 @@ def plot_colormap3dslice(filename=None,
             if np.ma.is_masked(maskgrid):
                 fSmap = fSmap[MaskX[0]:MaskX[-1]+1,:]
                 fSmap = fSmap[:,MaskY[0]:MaskY[-1]+1]
-            if np.ma.is_masked(rhomap):
+            if XYmask.any():
                 fSmap = np.ma.array(fSmap, mask=XYmask)            
             fScont = ax1.contour(XmeshCentres,YmeshCentres,fSmap,[0.5],colors=fScolour, 
                                  linestyles='solid',linewidths=0.5,zorder=2)
@@ -1066,7 +1070,7 @@ def plot_colormap3dslice(filename=None,
         if np.ma.is_masked(maskgrid):
             vectmap = vectmap[MaskX[0]:MaskX[-1]+1,:,:]
             vectmap = vectmap[:,MaskY[0]:MaskY[-1]+1,:]
-        if np.ma.is_masked(rhomap):
+        if XYmask.any():
             vectmap = np.ma.array(vectmap)
             for i in range(3):
                 vectmap[:,:,i].mask = XYmask
@@ -1133,7 +1137,7 @@ def plot_colormap3dslice(filename=None,
         if np.ma.is_masked(maskgrid):
             slinemap = slinemap[MaskX[0]:MaskX[-1]+1,:,:]
             slinemap = slinemap[:,MaskY[0]:MaskY[-1]+1,:]
-        if np.ma.is_masked(rhomap):
+        if XYmask.any():
             slinemap = np.ma.array(slinemap)
             for i in range(3):
                 slinemap[:,:,i].mask = XYmask