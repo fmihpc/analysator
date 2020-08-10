PTNOLATEX and precision fixes. Noted error in symlognorm base with older matplotlib versions.

pyPlots/plot.py
@@ -85,7 +85,7 @@ if os.getenv('PTOUTPUTDIR'):
 # nb: axis ticks are never plotted with scientific format
 def axisfmt(x, pos):
     f = r'{:.'+decimalprecision_ax+r'f}'
-    if os.getenv('PTNOLATEX') is None:
+    if not os.getenv('PTNOLATEX'):
         a = f.format(abs(x))
         if np.sign(x)<0: a = r'\mbox{\textbf{--}}'+a
         return r'$'+a+'$'
@@ -111,7 +111,7 @@ def cbfmtsci(x, pos):
     signchar=r'' 
     # Multiple braces for b take care of negative values in exponent
     # brackets around \times remove extra whitespace
-    if os.getenv('PTNOLATEX') is None:
+    if not os.getenv('PTNOLATEX'):
         # replaces minus sign with en-dash to fix big with latex descender value return
         if np.sign(x)<0: signchar=r'\mbox{\textbf{--}}'
     else:
@@ -138,7 +138,7 @@ def cbfmt(x, pos):
         precision = '0'
         if int(b)<1: precision = str(abs(int(b)))
     f = r'{:.'+precision+'f}'
-    if os.getenv('PTNOLATEX') is None:
+    if not os.getenv('PTNOLATEX'):
         a = f.format(abs(x))
         if np.sign(x)<0: a = r'\mbox{\textbf{--}}'+a
         return r'$'+a+'$'
@@ -148,7 +148,7 @@ def cbfmt(x, pos):
 
 # Helper routines for latex output handling    
 def bfstring(string):
-    if os.getenv('PTNOLATEX') is None:
+    if not os.getenv('PTNOLATEX'):
         if len(string)==0:
             return ''
         else:
@@ -157,7 +157,7 @@ def bfstring(string):
     return '{'+string+'}'
 
 def rmstring(string):
-    if os.getenv('PTNOLATEX') is None:
+    if not os.getenv('PTNOLATEX'):
         if len(string)==0:
             return ''
         else:
@@ -171,7 +171,7 @@ def mathmode(string):
     else:
         # First remove any internal possible dollar signs, then wrap whole string into math block
         result = string.replace('$','')
-        if os.getenv('PTNOLATEX') is None:
+        if not os.getenv('PTNOLATEX'):
             # Get rid of latex spaces
             result = result.replace('\,',' ')            
         return r"$"+result+"$"

pyPlots/plot_colormap.py
@@ -37,6 +37,7 @@ import matplotlib.ticker as mtick
 import colormaps as cmaps
 from matplotlib.cbook import get_sample_data
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from distutils.version import LooseVersion, StrictVersion
 
 def plot_colormap(filename=None,
                   vlsvobj=None,
@@ -763,7 +764,12 @@ def plot_colormap(filename=None,
     if lin is None:
         # Special SymLogNorm case
         if symlog is not None:
-            norm = SymLogNorm(base=10, linthresh=linthresh, linscale = 1.0, vmin=vminuse, vmax=vmaxuse, clip=True)
+            if LooseVersion(matplotlib.__version__) < LooseVersion("3.3.0"):
+                norm = SymLogNorm(linthresh=linthresh, linscale = 1.0, vmin=vminuse, vmax=vmaxuse, clip=True)
+                print("WARNING: colormap SymLogNorm uses base-e but ticks are calculated with base-10.")
+                #TODO: copy over matplotlib 3.3.0 implementation of SymLogNorm into pytools/analysator
+            else:
+                norm = SymLogNorm(base=10, linthresh=linthresh, linscale = 1.0, vmin=vminuse, vmax=vmaxuse, clip=True)
             maxlog=int(np.ceil(np.log10(vmaxuse)))
             minlog=int(np.ceil(np.log10(-vminuse)))
             logthresh=int(np.floor(np.log10(linthresh)))
@@ -1083,11 +1089,13 @@ def plot_colormap(filename=None,
 
         # Adjust precision for colorbar ticks
         thesetickvalues = cb.locator()
-        thesetickvalues = np.sort(np.abs(thesetickvalues[thesetickvalues != 0]))
         if len(thesetickvalues)<2:
             precision_b=1
         else:
-            mintickinterval = thesetickvalues[1]-thesetickvalues[0]
+            mintickinterval = abs(thesetickvalues[-1]-thesetickvalues[0])
+            # find smallest interval
+            for ticki in range(len(thesetickvalues)-1):
+                mintickinterval = min(mintickinterval,abs(thesetickvalues[ticki+1]-thesetickvalues[ticki]))
             precision_a, precision_b = '{:.1e}'.format(mintickinterval).split('e')
             # e.g. 9.0e-1 means we need precision 1
             # e.g. 1.33e-1 means we need precision 3?

pyPlots/plot_colormap3dslice.py
@@ -36,6 +36,7 @@ import matplotlib.ticker as mtick
 import colormaps as cmaps
 from matplotlib.cbook import get_sample_data
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from distutils.version import LooseVersion, StrictVersion
 import ids3d
 
 def plot_colormap3dslice(filename=None,
@@ -487,11 +488,6 @@ def plot_colormap3dslice(filename=None,
     # Scale data extent and plot box
     simext=[i/axisunit for i in simext]
     boxcoords=[i/axisunit for i in boxcoords]
-
-    # Set required decimal precision
-    precision_a, precision_b = '{:.1e}'.format(np.amax(abs(np.array(boxcoords)))).split('e')
-    pt.plot.decimalprecision_ax = '0'
-    if int(precision_b)<1: pt.plot.decimalprecision_ax = str(abs(-1-int(precision_b)))
 
     #################################################
     # Find rhom map for use in masking out ionosphere
@@ -976,7 +972,12 @@ def plot_colormap3dslice(filename=None,
     if lin is None:
         # Special SymLogNorm case
         if symlog is not None:
-            norm = SymLogNorm(base=10, linthresh=linthresh, linscale = 0.3, vmin=vminuse, vmax=vmaxuse, clip=True)
+            if LooseVersion(matplotlib.__version__) < LooseVersion("3.3.0"):
+                norm = SymLogNorm(linthresh=linthresh, linscale = 1.0, vmin=vminuse, vmax=vmaxuse, clip=True)
+                print("WARNING: colormap SymLogNorm uses base-e but ticks are calculated with base-10.")
+                #TODO: copy over matplotlib 3.3.0 implementation of SymLogNorm into pytools/analysator
+            else:
+                norm = SymLogNorm(base=10, linthresh=linthresh, linscale = 1.0, vmin=vminuse, vmax=vmaxuse, clip=True)
             maxlog=int(np.ceil(np.log10(vmaxuse)))
             minlog=int(np.ceil(np.log10(-vminuse)))
             logthresh=int(np.floor(np.log10(linthresh)))
@@ -1049,7 +1050,7 @@ def plot_colormap3dslice(filename=None,
 
     # Title and plot limits
     if len(plot_title)!=0:
-        if os.getenv('PTNOLATEX') is None:  # This should probably check value...
+        if not os.getenv('PTNOLATEX'):  # This should probably check value...
             # Add 3D slice position in title
             plot_title = r"\textbf{"+slicelabel+plot_title+"}"
         ax1.set_title(plot_title,fontsize=fontsize2,fontweight='bold')
@@ -1066,7 +1067,7 @@ def plot_colormap3dslice(filename=None,
     #ax1.yaxis.set_tick_params(which='minor',width=3,length=5)
 
     if not noxlabels:
-        if os.getenv('PTNOLATEX') is None:
+        if not os.getenv('PTNOLATEX'):
             xlabelstr = r'\textbf{'+axislabels[0]+' ['+axisunitstr+']}'
         else:
             xlabelstr = r''+axislabels[0]+' ['+axisunitstr+']'
@@ -1076,7 +1077,7 @@ def plot_colormap3dslice(filename=None,
             item.set_fontweight('black')
         ax1.xaxis.offsetText.set_fontsize(fontsize)# set axis exponent offset font sizes
     if not noylabels:
-        if os.getenv('PTNOLATEX') is None:
+        if not os.getenv('PTNOLATEX'):
             ylabelstr = r'\textbf{'+axislabels[1]+' ['+axisunitstr+']}'
         else:
             ylabelstr = r''+axislabels[1]+' ['+axisunitstr+']'
@@ -1256,7 +1257,7 @@ def plot_colormap3dslice(filename=None,
 
         # Colourbar title
         if len(cb_title_use)!=0:
-            if os.getenv('PTNOLATEX') is not None:
+            if os.getenv('PTNOLATEX'):
                 cb_title_use.replace('\textbf{','')
                 cb_title_use.replace('\mathrm{','')
                 cb_title_use.replace('}','')
@@ -1309,11 +1310,13 @@ def plot_colormap3dslice(filename=None,
 
         # Adjust precision for colorbar ticks
         thesetickvalues = cb.locator()
-        thesetickvalues = np.sort(np.abs(thesetickvalues[thesetickvalues != 0]))
         if len(thesetickvalues)<2:
             precision_b=1
         else:
-            mintickinterval = thesetickvalues[1]-thesetickvalues[0]
+            mintickinterval = abs(thesetickvalues[-1]-thesetickvalues[0])
+            # find smallest interval
+            for ticki in range(len(thesetickvalues)-1):
+                mintickinterval = min(mintickinterval,abs(thesetickvalues[ticki+1]-thesetickvalues[ticki]))
             precision_a, precision_b = '{:.1e}'.format(mintickinterval).split('e')
             # e.g. 9.0e-1 means we need precision 1
             # e.g. 1.33e-1 means we need precision 3?

pyPlots/plot_isosurface.py
@@ -295,7 +295,7 @@ def plot_isosurface(filename=None,
             unitstr = r'$10^{'+str(int(unit))+'}$ m'
         unit = np.power(10,int(unit))
     else:
-        if os.getenv('PTNOLATEX') is None:
+        if not os.getenv('PTNOLATEX'):
             unitstr = r'$\mathrm{R}_{\mathrm{E}}$'
         else:
             unitstr = r'$R_E$'