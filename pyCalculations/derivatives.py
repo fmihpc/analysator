Derivatives who may not be correct yet, bounding box reader functions, etc

pyCalculations/derivatives.py
@@ -27,7 +27,7 @@ import pytools as pt
 import warnings
 from scipy import interpolate
 
-def get_fg_divPoynting:
+def fg_PoyntingFlux(bulkReader):
    b = bulkReader.read_fg_variable_as_volumetric('fg_b')
    print('b.shape=',b.shape)
    e = bulkReader.read_fg_variable_as_volumetric('fg_e')
@@ -35,18 +35,34 @@ def get_fg_divPoynting:
 
    mu_0 = 1.25663706144e-6
    S = np.cross(e,b)/mu_0
-   print('S.shape', S.shape)
+   return S
+
+
+def fg_divPoynting(bulkReader, dx=1e6):
+   S = fg_PoyntingFlux(bulkReader)
 
    #divS = pt.plot.plot_helpers.numdiv(pt.plot.plot_helpers.TransposeVectorArray(np.squeeze(S))).T
    divS = (np.roll(S[:,:,:,0],-1, 0) - np.roll(S[:,:,:,0], 1, 0) +
          np.roll(S[:,:,:,1],-1, 1) - np.roll(S[:,:,:,1], 1, 1) +
-         np.roll(S[:,:,:,2],-1, 2) - np.roll(S[:,:,:,2], 1, 2))/(2*1e6)
+         np.roll(S[:,:,:,2],-1, 2) - np.roll(S[:,:,:,2], 1, 2))/(2*dx)
    print('divS.shape', divS.shape)
    #divS = np.div
 
-   fig=plt.pyplot.figure()
-   ax=plt.pyplot.axes()
-   im=ax.imshow(divS[:,int(divS.shape[1]/2),:], origin='lower', norm=plt.colors.SymLogNorm(base=10, linthresh=1e-12, linscale = 1.0, vmin=-1e-7, vmax=1e-7, clip=True), cmap='vik')
-   plt.pyplot.colorbar(im, ax=ax)
-   plt.pyplot.savefig('./divS_centered.png')
-   print(divS)
+#   print(divS)
+   return divS
+
+def fg_vol_jacobian(array, dx=1e6):
+   dFx_dx, dFx_dy, dFx_dz = np.gradient(array[:,:,:,0], dx)
+   dFy_dx, dFy_dy, dFy_dz = np.gradient(array[:,:,:,1], dx)
+   dFz_dx, dFz_dy, dFz_dz = np.gradient(array[:,:,:,2], dx)
+
+def fg_vol_curl(array, dx=1e6):
+   dummy,  dFx_dy, dFx_dz = np.gradient(array[:,:,:,0], dx)
+   dFy_dx, dummy,  dFy_dz = np.gradient(array[:,:,:,1], dx)
+   dFz_dx, dFz_dy, dummy  = np.gradient(array[:,:,:,2], dx)
+
+   rotx = dFz_dy - dFy_dz
+   roty = dFx_dz - dFz_dx
+   rotz = dFy_dx - dFx_dy
+
+   return np.stack([rotx, roty, rotz], axis=-1)

pyVlsv/vlsvreader.py
@@ -1082,8 +1082,8 @@ class VlsvReader(object):
       print('read in fgdata with shape', fgdata.shape, name)
       celldata = np.zeros_like(fgdata)
       known_centerings = {"fg_b":"face", "fg_e":"edge"}
-      if name.casefold() in known_centerings.keys() and centering == "default":
-         centering = known_centerings[name.casefold()]
+      if name.lower() in known_centerings.keys() and centering == "default":
+         centering = known_centerings[name.lower()]
       else:
          print("A variable with unknown centering! Aborting.")
          return False
@@ -1264,7 +1264,13 @@ class VlsvReader(object):
       lowi, upi = self.get_fsgrid_slice_indices(low, up)
       return lowi, upi
 
-   def get_fsgrid_subarray(self, cellid, array):
+   def get_bbox_fsgrid_slicemap(self, low, up):
+      '''Returns a slice tuple of fsgrid indices that are contained in the (low, up) bounding box.
+      '''
+      lowi, upi = self.get_fsgrid_slice_indices(low, up)
+      return lowi, upi
+
+   def get_cell_fsgrid_subarray(self, cellid, array):
       '''Returns a subarray of the fsgrid array, corresponding to the fsgrid
       covered by the SpatialGrid cellid. [untested]
       '''
@@ -1275,11 +1281,21 @@ class VlsvReader(object):
       else:
          return array[lowi[0]:upi[0]+1, lowi[1]:upi[1]+1, lowi[2]:upi[2]+1]
 
+   def get_bbox_fsgrid_subarray(self, low, up, array):
+      '''Returns a subarray of the fsgrid array, corresponding to the (low, up) bounding box.
+      '''
+      lowi, upi = self.get_bbox_fsgrid_slicemap(low,up)
+      #print('subarray:',lowi, upi)
+      if array.ndim == 4:
+         return array[lowi[0]:upi[0]+1, lowi[1]:upi[1]+1, lowi[2]:upi[2]+1, :]
+      else:
+         return array[lowi[0]:upi[0]+1, lowi[1]:upi[1]+1, lowi[2]:upi[2]+1]
+
 
    def downsample_fsgrid_subarray(self, cellid, array):
       '''Returns a mean value of fsgrid values underlying the SpatialGrid cellid.
       '''
-      fsarr = self.get_fsgrid_subarray(cellid, array)
+      fsarr = self.get_cell_fsgrid_subarray(cellid, array)
       n = fsarr.size
       if fsarr.ndim == 4:
          n = n/3
@@ -1290,8 +1306,7 @@ class VlsvReader(object):
 
    def upsample_fsgrid_subarray(self, cellid, var, array):
       '''Set the elements of the fsgrid array to the value of corresponding SpatialGrid
-      cellid. Mutator.
-      [untested]
+      cellid. Mutator for array.
       '''
       lowi, upi = self.get_cell_fsgrid_slicemap(cellid)
       #print(lowi,upi)