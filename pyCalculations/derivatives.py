9386f1d5f8db67f7231a1f3586e176e07284ec98
Markku Alho <markku.alho@helsinki.fi>
2021-09-08 13:07:04 +0300

    Added some of Jonas' helpers and ballooning criterion.

--- pyCalculations/derivatives.py
+++ pyCalculations/derivatives.py
@@ -41,20 +41,19 @@ def fg_PoyntingFlux(bulkReader):
 def fg_divPoynting(bulkReader, dx=1e6):
    S = fg_PoyntingFlux(bulkReader)
 
-   #divS = pt.plot.plot_helpers.numdiv(pt.plot.plot_helpers.TransposeVectorArray(np.squeeze(S))).T
    divS = (np.roll(S[:,:,:,0],-1, 0) - np.roll(S[:,:,:,0], 1, 0) +
          np.roll(S[:,:,:,1],-1, 1) - np.roll(S[:,:,:,1], 1, 1) +
          np.roll(S[:,:,:,2],-1, 2) - np.roll(S[:,:,:,2], 1, 2))/(2*dx)
    print('divS.shape', divS.shape)
-   #divS = np.div
 
-#   print(divS)
    return divS
 
-def fg_vol_jacobian(array, dx=1e6):
-   dFx_dx, dFx_dy, dFx_dz = np.gradient(array[:,:,:,0], dx)
-   dFy_dx, dFy_dy, dFy_dz = np.gradient(array[:,:,:,1], dx)
-   dFz_dx, dFz_dy, dFz_dz = np.gradient(array[:,:,:,2], dx)
+def fg_vol_jacobian(b, dx=1e6):
+   dFx_dx, dFx_dy, dFx_dz = np.gradient(b[:,:,:,0], dx)
+   dFy_dx, dFy_dy, dFy_dz = np.gradient(b[:,:,:,1], dx)
+   dFz_dx, dFz_dy, dFz_dz = np.gradient(b[:,:,:,2], dx)
+
+   return np.stack()
 
 def fg_vol_curl(array, dx=1e6):
    dummy,  dFx_dy, dFx_dz = np.gradient(array[:,:,:,0], dx)
@@ -66,3 +65,66 @@ def fg_vol_curl(array, dx=1e6):
    rotz = dFy_dx - dFx_dy
 
    return np.stack([rotx, roty, rotz], axis=-1)
+
+def fg_vol_div(array, dx=1e6):
+   dFx_dx, dummy, dummy = np.gradient(array[:,:,:,0], dx)
+   dummy, dFy_dy, dummy = np.gradient(array[:,:,:,1], dx)
+   dummy, dummy, dFz_dz = np.gradient(array[:,:,:,2], dx)
+
+   return dFx_dx+dFy_dy+dFz_dz
+
+def vfield3_dot(a, b):
+    """Calculates dot product of vectors a and b in 3D vector field"""
+
+    return (
+        a[:, :, :, 0] * b[:, :, :, 0]
+        + a[:, :, :, 1] * b[:, :, :, 1]
+        + a[:, :, :, 2] * b[:, :, :, 2]
+    )
+
+def vfield3_matder(a, b, dr):
+    """Calculates material derivative of 3D vector fields a and b"""
+
+    bx = b[:, :, :, 0]
+    by = b[:, :, :, 1]
+    bz = b[:, :, :, 2]
+
+    grad_bx = np.gradient(bx, dr)
+    grad_by = np.gradient(by, dr)
+    grad_bz = np.gradient(bz, dr)
+
+    resx = vfield3_dot(a, grad_bx)
+    resy = vfield3_dot(a, grad_by)
+    resz = vfield3_dot(a, grad_bz)
+
+    return np.stack((resx, resy, resz), axis=-1)
+
+def vfield3_normalise(a):
+
+    amag = np.linalg.norm(a, axis=-1)
+
+    resx = a[:, :, :, 0] / amag
+    resy = a[:, :, :, 1] / amag
+    resz = a[:, :, :, 2] / amag
+
+    return np.stack((resx, resy, resz), axis=-1)
+
+def ballooning_crit(B, P, beta, dr=1000e3):
+
+    # Bmag = np.linalg.norm(B, axis=-1)
+
+    b = vfield3_normalise(B)
+
+    n = vfield3_matder(b, b, dr)
+
+    nnorm = vfield3_normalise(n)
+
+    gradP = np.gradient(P,dr)
+
+    kappaP = vfield3_dot(nnorm, gradP) / P
+    
+    kappaC = vfield3_dot(nnorm, n)
+
+    balloon = (2 + beta) / 4.0 * kappaP / (kappaC + 1e-27)
+
+    return (balloon, nnorm, kappaC, gradP)
