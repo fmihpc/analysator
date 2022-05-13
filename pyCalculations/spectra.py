Fixes to spectra integration, defaults. Removed modvelocity spectra, flux weighting for alongaxis-velocity; feel free to reimplement if those are needed, _in the specific manner required_.

pyCalculations/spectra.py
@@ -57,97 +57,44 @@ def get_spectrum_energy(vlsvReader,
       f = np.asarray(f[1])
    else:
       return (False,np.zeros(nBins), EkinBinEdges)
-   ii_f = np.where(f >= fMin)
+   ii_f = np.where(np.logical_and(f >= fMin, Ekin > 0))
    if len(ii_f) < 1:
       return (False,np.zeros(nBins), EkinBinEdges)
    f = f[ii_f]
    Ekin = Ekin[ii_f]
    V2 = V2[ii_f]
 
-   Ekin[Ekin < min(EkinBinEdges)] = min(EkinBinEdges)
-   Ekin[Ekin > max(EkinBinEdges)] = max(EkinBinEdges)
+   dV3 = np.prod(vlsvReader.get_velocity_mesh_dv(population))
    # normalization
    if(weight == 'flux'):
-      fw = f*np.sqrt(V2) # use particle flux as weighting
-      units = "s/(m^2 4pi eV)"
-      latexunits = '$\mathrm{s}\,\mathrm{m}^{-2}\,(4\pi\,\mathrm{eV})^{-1}$'
-      latex='$f(\vec{r})v\,\DeltaE\,sr^-1$'
+      fw = f * np.sqrt(V2) * dV3 / (4*np.pi * Ekin) # use particle flux as weighting
+      units = "1/(s m^2 sr eV)"
+      latexunits = '$\mathrm{s}^-1\,\mathrm{m}^{-2}\,\mathrm{sr}^{-1}\,\mathrm{eV}^{-1}$'
+      latex='$\mathcal{F}(\vec{r},E)$'
    else:
-      fw = f
-      units = "1/(m^3 4pi eV)"
-      latexunits = '$\mathrm{m}^{-3}\,(4\pi\,\mathrm{eV})^{-1}$'
-      latex='$f(\vec{r})\,\DeltaE^-1\,sr^-1$'
+      fw = f * dV3 / Ekin
+      units = "1/(m^3 eV)"
+      latexunits = '$\mathrm{m}^{-3}\,\mathrm{eV}^{-1}$'
+      latex='$f(\vec{r},E)$'
       weight = 'particles'
-   dV = np.prod(vlsvReader.get_velocity_mesh_dv(population))
+
+   #Ekin[Ekin < min(EkinBinEdges)] = min(EkinBinEdges)
+   #Ekin[Ekin > max(EkinBinEdges)] = max(EkinBinEdges)
+
    # compute histogram
-   (nhist,edges) = np.histogram(Ekin,bins=EkinBinEdges,weights=fw*dV,normed=0)
-   # normalization
-   ftotal = np.sum(f)*dV
-   #print('ftotal', ftotal, 'sum hist', np.sum(nhist))
+   (nhist,edges) = np.histogram(Ekin,bins=EkinBinEdges,weights=fw,normed=0)
+
    dE = EkinBinEdges[1:] - EkinBinEdges[0:-1]
-   nhist = np.divide(nhist,(dE*4*np.pi))
+   #nhist = np.divide(nhist,dE)
    vari = pytools.calculations.VariableInfo(nhist,
-                                       name="Omnidirectional energy spectrum "+population+' ('+weight+')',
+                                       name="Omnidirectional differential energy spectrum "+population+' ('+weight+')',
                                        units=units,
                                        latex=latex,
                                        latexunits=latexunits)
    return (True,vari,edges)
 
 
-# Function to reduce the velocity space in a spatial cell to an omnidirectional speed spectrum
-def get_spectrum_modvelocity(vlsvReader,
-                  cid,
-                  population="proton",
-                  fMin = 1e-21,
-                  VMin=100,
-                  VMax=2e6,
-                  nBins=66,
-                  frame=None,
-                  weight='flux',
-                  restart=True):
-
-   VBinEdges = np.logspace(np.log10(VMin),np.log10(VMax),nBins)
-   vlsvReader = pytools.vlsvfile.VlsvReader(vlsvReader)
-   # check if velocity space exists in this cell
-   if not restart and vlsvReader.read_variable('fSaved',cid) != 1.0:
-      return (False,np.zeros(nBins), VBinEdges)
-   if vlsvReader.check_variable('MinValue') == True:
-      fMin = vlsvReader.read_variable('MinValue',cid)
-   #print('Cell ' + str(cid).zfill(9))
-   velcells = vlsvReader.read_velocity_cells(cid, population)
-   V = vlsvReader.get_velocity_cell_coordinates(list(velcells.keys()), pop=population)
-   V2 = np.sum(np.square(V),1)
-   Vmod = np.sqrt(V2)
-   f = list(zip(*velcells.items()))
-
-
-   # check that velocity space has cells - still return a zero histogram in the same shape
-   if(len(f) > 0):
-      f = np.asarray(f[1])
-   else:
-      return (False,np.zeros(nBins), VBinEdges)
-   ii_f = np.where(f >= fMin)
-   if len(ii_f) < 1:
-      return (False,np.zeros(nBins), VBinEdges)
-   f = f[ii_f]
-   Vmod = Vmod[ii_f]
-   V2 = V2[ii_f]
-
-   Vmod[Vmod < min(VBinEdges)] = min(VBinEdges)
-   Vmod[Vmod > max(VBinEdges)] = max(VBinEdges)
-   # normalization
-   if(weight == 'flux'):
-      fw = f*np.sqrt(V2) # use particle flux as weighting
-   else:
-      fw = f
-   # compute histogram
-   (nhist,edges) = np.histogram(Vmod,bins=VBinEdges,weights=fw,normed=0)
-   # normalization
-   dv = VBinEdges[1:] - VBinEdges[0:-1]
-   nhist = np.divide(nhist,(dv*4*np.pi))
-   return (True,nhist,edges)
-
-# Function to reduce the velocity space in a spatial cell to an spectrum in velocity along a vector
+# Function to reduce the velocity space in a spatial cell to a distribution in velocity along a vector
 def get_spectrum_alongaxis_vel(vlsvReader,
                   cid,
                   population="proton",
@@ -158,7 +105,6 @@ def get_spectrum_alongaxis_vel(vlsvReader,
                   VMax=2e6,
                   nBins=200,
                   frame=None,
-                  weight='flux',
                   restart=True):
 
    vlsvReader = pytools.vlsvfile.VlsvReader(vlsvReader)
@@ -195,31 +141,21 @@ def get_spectrum_alongaxis_vel(vlsvReader,
 
    Vproj[Vproj < min(VBinEdges)] = min(VBinEdges)
    Vproj[Vproj > max(VBinEdges)] = max(VBinEdges)
+   dV3 = np.prod(vlsvReader.get_velocity_mesh_dv(population))
    # normalization
-   # normalization
-   if(weight == 'flux'):
-      fw = f*np.sqrt(V2) # use particle flux as weighting
-      units = "s^2/(m^3)"
-      latexunits = '$\mathrm{s}\,\mathrm{m}^{-4}$'
-      latex='$f(\vec{r})\,\Deltav^-1$'
-   else:
-      units = "s/m^4"
-      latexunits = '$\mathrm{s}\mathrm{m}^{-4}$'
-      latex='$f(\vec{r})\,\Deltav^-1\$'
-      weight = 'particles'
-      fw = f
+   units = "s/m^4"
+   latexunits = '$\mathrm{s}\mathrm{m}^{-4}$'
+   latex='$f(\vec{r})\,\Deltav^-1\$'
+   weight = 'particles'
+   fw = f*dV3
    
-   # compute histogram
-   #print(fw)
-   #print(len(Vproj), len(fw))
-   dV = np.prod(vlsvReader.get_velocity_mesh_dv(population))
-   (nhist,edges) = np.histogram(Vproj,bins=VBinEdges,weights=fw*dV,normed=0)
+   (nhist,edges) = np.histogram(Vproj,bins=VBinEdges,weights=fw,normed=0)
    # normalization
    dv = VBinEdges[1:] - VBinEdges[0:-1]
    nhist = np.divide(nhist,dv)
    
    vari = pytools.calculations.VariableInfo(nhist,
-                                    name="Parallel spectrum of "+population+' ('+weight+')',
+                                    name="Parallel distribution of "+population+' ('+weight+')',
                                     units=units,
                                     latex=latex,
                                     latexunits=latexunits)

pyPlots/plot_vdf.py
@@ -72,7 +72,7 @@ def verifyCellWithVspace(vlsvReader,cid):
 def doHistogram(f,VX,VY,Voutofslice,vxBinEdges,vyBinEdges,vthick,reducer="integrate", wflux=None, initial_dV=1.0):
     # Flux weighting?
     if wflux is not None:
-        fw = f*np.linalg.norm([VX,VY,Voutofslice]) # use particle flux as weighting in the histogram
+        fw = f*np.linalg.norm([VX,VY,Voutofslice])/(4*np.pi) # use particle flux as weighting in the histogram
     else:
         fw = f # use particle phase-space density as weighting in the histogram
 
@@ -87,16 +87,13 @@ def doHistogram(f,VX,VY,Voutofslice,vxBinEdges,vyBinEdges,vthick,reducer="integr
 
     # Gather histogram of values
     (nVhist,VXEdges,VYEdges) = np.histogram2d(VX[tuple(indexes)],VY[tuple(indexes)],bins=(vxBinEdges,vyBinEdges),weights=fw[tuple(indexes)],normed=0)
-    # Gather histogram of how many cells were summed for the histogram
-    (Chist,VXEdges,VYEdges) = np.histogram2d(VX[tuple(indexes)],VY[tuple(indexes)],bins=(vxBinEdges,vyBinEdges),normed=0)
 
     # Correct for summing multiple cells into one histogram output cell with the averaging reducer
     if reducer == "average":
-        print("averaging for sampling errors")
+        # Gather histogram of how many cells were summed for the histogram
+        (Chist,VXEdges,VYEdges) = np.histogram2d(VX[tuple(indexes)],VY[tuple(indexes)],bins=(vxBinEdges,vyBinEdges),normed=0)
         nonzero = np.where(Chist != 0)
-        print(Chist[nonzero])
         nonzero = Chist > 0
-        print(Chist[nonzero])
         nVhist[nonzero] = np.divide(nVhist[nonzero],Chist[nonzero])
 
     dV = np.abs(vxBinEdges[-1] - vxBinEdges[-2]) # assumes constant bin size
@@ -118,11 +115,6 @@ def doHistogram(f,VX,VY,Voutofslice,vxBinEdges,vyBinEdges,vthick,reducer="integr
     # and y along the second dimension of the array (horizontal). This ensures compatibility with histogramdd.
     nVhist = nVhist.transpose()
     
-
-    # Flux weighting
-    if wflux is not None:
-        nVhist = np.divide(nVhist,(dV*4*np.pi)) # normalization
-
     # Rotation contributions are already normalized, this is just to rescaling to correct units
     # nb: maybe not great for finite slices...?
     if reducer == "integrate":
@@ -131,10 +123,10 @@ def doHistogram(f,VX,VY,Voutofslice,vxBinEdges,vyBinEdges,vthick,reducer="integr
     return (nVhist,VXEdges,VYEdges)
   
 def resampleReducer(V,f, inputcellsize, setThreshold, normvect, normvectX, slicetype, slicethick, reducer="integrate"):
-    NX = np.array(normvect)/np.sqrt(normvect[0]**2 + normvect[1]**2 + normvect[2]**2)
-    NY = np.array(normvectX)/np.sqrt(normvectX[0]**2 + normvectX[1]**2 + normvectX[2]**2)
+    NX = np.array(normvect)/np.linalg.norm(normvect)
+    NY = np.array(normvectX)/np.linalg.norm(normvectX)
     NZ = np.cross(NX,NY)
-    NY = np.array(NY)/np.sqrt(NY[0]**2 + NY[1]**2 + NY[2]**2)
+    NY = np.array(NY)
     if slicetype=="Bpara":
         R = np.stack((NX, NY, NZ)).T
     elif slicetype=="Bpara1":
@@ -464,7 +456,7 @@ def plot_vdf(filename=None,
 
     :kword box:         extents of plotted velocity grid as [x0,x1,y0,y1] (in m/s)
     :kword axisunit:    Plot v-axes using 10^{axisunit} m/s (default: km/s)
-    :kword axiskmps:    Plot v-axes using 10^{axisunit} km/s (default: m/s)
+    :kword axiskmps:    Plot v-axes using 10^{axiskmps} km/s (default: km/s, when the kword has a value)
     :kword tickinterval: Interval at which to have ticks on axes
    
     :kword xy:          Perform slice in x-y-direction
@@ -1245,7 +1237,7 @@ def plot_vdf(filename=None,
                     if reducer == 'average':
                         cb_title_use=r"flux $F\,["+pt.plot.rmstring('m')+"^{-2} \,"+pt.plot.rmstring('s')+"^{-1} \,"+pt.plot.rmstring('sr')+"^{-1}]$"
                     elif reducer == 'integrate':
-                        cb_title_use=r"flux $F\,["+pt.plot.rmstring('m')+"^{-2} \,"+pt.plot.rmstring('s')+"^{-1} \,"+pt.plot.rmstring('sr')+"^{-1}\,"+pt.plot.rmstring('m')+"\,"+pt.plot.rmstring('s')+"^{-1}]$"
+                        cb_title_use=r"flux $F\,["+pt.plot.rmstring('m')+"^{-1} \,"+pt.plot.rmstring('s')+"^{-2} \,"+pt.plot.rmstring('sr')+"^{-1}]$"
 
             if cbaxes is not None:
                 cax = cbaxes