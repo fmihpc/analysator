76d092fee7a7c459efaff8cea44742945c8e06b4
Markku Alho <markku.alho@helsinki.fi>
2023-06-21 08:57:43 +0300

    Variable order reconstruction with reduced memory footprint. Performance for EGI fsgrid ops ~quadrupled. Still to encapsulate this into a reconstructor class.

--- scripts/divergenceless_reconstruction.py
+++ scripts/divergenceless_reconstruction.py
@@ -3,13 +3,13 @@ import numpy as np
 from scipy.special import legendre
 
 P = tuple(map(lambda n: legendre(n), (0, 1, 2, 3, 4)))
-
+order = 2
 # y and z are simply cyclic rotations of this
 def interpolate_x(a, x, y, z):
     B_x = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_x += a[i][j][k] * P[i](x) * P[j](y) * P[k](z)
     return B_x
 
@@ -22,78 +22,78 @@ def interpolate_z(c, x, y, z):
 # y and z are simply cyclic rotations of this
 def interpolate_dbxdx(a, x, y, z):
     B_x = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_x += a[i][j][k] * np.polyder(P[i])(x) * P[j](y) * P[k](z)
     return B_x
 
 def interpolate_dbydx(b, x, y, z):
     B_y = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_y += b[i][j][k] * P[i](y) * P[j](z) * np.polyder(P[k])(x)
     return B_y
     #return interpolate_dbxdx(b, y, z, x)
 
 def interpolate_dbzdx(c, x, y, z):
     B_z = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_z += c[i][j][k] * P[i](z) * np.polyder(P[j])(x) * P[k](y)
     return B_z
     #return interpolate_dbxdx(c, z, x, y)
 
 def interpolate_dbxdy(a, x, y, z):
     B_x = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_x += a[i][j][k] * P[i](x) * np.polyder(P[j])(y) * P[k](z)
     return B_x
 
 def interpolate_dbydy(b, x, y, z):
     B_y = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_y += b[i][j][k] * np.polyder(P[i])(y) * P[j](z) * P[k](x)
     return B_y
     #return interpolate_dbxdy(b, y, z, x)
 
 def interpolate_dbzdy(c, x, y, z):
     B_z = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_z += c[i][j][k] * P[i](z) * P[j](x) * np.polyder(P[k])(y)
     return B_z
     # return interpolate_dbxdy(c, z, x, y)
 
 def interpolate_dbxdz(a, x, y, z):
     B_x = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_x += a[i][j][k] * P[i](x) * P[j](y) * np.polyder(P[k])(z)
     return B_x
 
 def interpolate_dbydz(b, x, y, z):
     B_y = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_y += b[i][j][k] * P[i](y) * np.polyder(P[j])(z) * P[k](x)
     return B_y
     # return interpolate_dbxdz(b, y, z, x)
 
 def interpolate_dbzdz(c, x, y, z):
     B_z = 0
-    for i in range(5):
-        for j in range(4):
-            for k in range(4):
+    for i in range(order+1):
+        for j in range(order):
+            for k in range(order):
                 B_z += c[i][j][k] * np.polyder(P[i])(z) * P[j](x) * P[k](y)
     return B_z
     # return interpolate_dbxdz(c, z, x, y)
@@ -133,8 +133,8 @@ def solve_z_moments(B_z):
 # Solves a, b, c components with a[x, y, z], b[y, z, x] and c[z, x, y] up to given order
 # Input B should be the output of solve_moments_from_B
 # Might be deprecated. A lot faster than calculating all coefficients but that's only a few seconds anyway for a 100^3 array
-def solve_coefficients(B_moments, xyz, order = 4):
-    abc = np.zeros((3, 5, 4, 4))
+def solve_coefficients(B_moments, xyz):
+    abc = np.zeros((3, order+1, order, order))
     x = xyz[0]
     y = xyz[1]
     z = xyz[2]
@@ -213,8 +213,12 @@ def solve_coefficients(B_moments, xyz, order = 4):
             a[0][1][0] = 1/2 * (Bx[1][xyz] + Bx[1][coords]) - 1/6 * a[2][1][0]
             a[0][0][1] = 1/2 * (Bx[2][xyz] + Bx[2][coords]) - 1/6 * a[2][0][1]
 
-            a[1][1][0] = (Bx[1][xyz] - Bx[1][coords]) - 1/10 * a[3][1][0]
-            a[1][0][1] = (Bx[2][xyz] - Bx[2][coords]) - 1/10 * a[3][0][1]
+            a[1][1][0] = (Bx[1][xyz] - Bx[1][coords])
+            if order > 2:
+                a[1][1][0] -= 1/10 * a[3][1][0]
+            a[1][0][1] = (Bx[2][xyz] - Bx[2][coords]) 
+            if order > 2:
+                a[1][1][0] -= 1/10 * a[3][0][1]
         
         for i in range(3):
             j = (i+1) % 3
@@ -224,7 +228,11 @@ def solve_coefficients(B_moments, xyz, order = 4):
             c = abc[k]
 
             # Should be correct, check later
-            a[2][0][0] = -1/2 * (b[1][0][1] + c[1][1][0]) - 3/35 * a[4][0][0] - 1/20 * (b[3][0][1] + c[3][1][0])
+            a[2][0][0] = -1/2 * (b[1][0][1] + c[1][1][0])
+            if order > 3: 
+                a[2][0][0] -= 3/35 * a[4][0][0] 
+            if order > 2:
+                a[2][0][0] -= 1/20 * (b[3][0][1] + c[3][1][0])
 
     # 1st order
     if (order > 0):
@@ -235,7 +243,9 @@ def solve_coefficients(B_moments, xyz, order = 4):
             a = abc[i]
             Bx = B_moments[i]
 
-            a[1][0][0] = (Bx[0][xyz] - Bx[0][coords]) - 1/10 * a[3][0][0]
+            a[1][0][0] = (Bx[0][xyz] - Bx[0][coords])
+            if order > 2:
+                 a[1][0][0] -= 1/10 * a[3][0][0]
     
     # 0th order
     for i in range(3):
@@ -245,10 +255,14 @@ def solve_coefficients(B_moments, xyz, order = 4):
         a = abc[i]
         Bx = B_moments[i]
 
-        a[0][0][0] = 1/2 * (Bx[0, x, y, z] + Bx[0][coords]) - 1/6 * a[2][0][0] - 1/70 * a[4][0][0]
+        a[0][0][0] = 1/2 * (Bx[0, x, y, z] + Bx[0][coords])
+        if order > 1:
+            a[0][0][0] -= 1/6 * a[2][0][0] 
+        if order > 3:
+            a[0][0][0] -= 1/70 * a[4][0][0]
 
     # Check constraint:
-    test = abc[0][1][0][0] + abc[1][1][0][0] + abc[2][1][0][0] + 1/10 * (abc[0][3][0][0] + abc[1][3][0][0] + abc[2][3][0][0])
+    #test = abc[0][1][0][0] + abc[1][1][0][0] + abc[2][1][0][0] + 1/10 * (abc[0][3][0][0] + abc[1][3][0][0] + abc[2][3][0][0])
     #if abs(test) > 0:
     #    print("Something went wrong, sum (17) is " + str(test))
 
@@ -276,9 +290,9 @@ def neighbourdiff(a, idx):
 
 # Solves a, b, c components with a[x, y, z], b[y, z, x] and c[z, x, y] up to given order
 # Input B should be the output of solve_moments_from_B
-def solve_all_coefficients(B_moments, order = 4):
+def solve_all_coefficients(B_moments):
     shp = np.shape(B_moments[0][0])
-    abc = np.zeros((3, 5, 4, 4, shp[0] - 1, shp[1] - 1, shp[2] - 1))
+    abc = np.zeros((3, order+1, order, order, shp[0] - 1, shp[1] - 1, shp[2] - 1))
 
     # 4th order
     if (order > 3):
@@ -346,8 +360,12 @@ def solve_all_coefficients(B_moments, order = 4):
             a[0][1][0] = 1/2 * neighboursum(Bx[1], i) - 1/6 * a[2][1][0]
             a[0][0][1] = 1/2 * neighboursum(Bx[2], i) - 1/6 * a[2][0][1]
 
-            a[1][1][0] = neighbourdiff(Bx[1], i) - 1/10 * a[3][1][0]
-            a[1][0][1] = neighbourdiff(Bx[2], i) - 1/10 * a[3][0][1]
+            a[1][1][0] = neighbourdiff(Bx[1], i)
+            if order > 2:
+                a[1][1][0] -= 1/10 * a[3][1][0]
+            a[1][0][1] = neighbourdiff(Bx[2], i) 
+            if order > 2:
+                a[1][0][1] -= 1/10 * a[3][0][1]
         
         for i in range(3):
             j = (i+1) % 3
@@ -357,7 +375,11 @@ def solve_all_coefficients(B_moments, order = 4):
             c = abc[k]
 
             # Should be correct, check later
-            a[2][0][0] = -1/2 * (b[1][0][1] + c[1][1][0]) - 3/35 * a[4][0][0] - 1/20 * (b[3][0][1] + c[3][1][0])
+            a[2][0][0] = -1/2 * (b[1][0][1] + c[1][1][0])
+            if order > 3:
+                a[2][0][0] -= 3/35 * a[4][0][0]
+            if order > 2:
+                a[2][0][0] -= 1/20 * (b[3][0][1] + c[3][1][0])
 
     # 1st order
     if (order > 0):
@@ -365,14 +387,20 @@ def solve_all_coefficients(B_moments, order = 4):
             a = abc[i]
             Bx = B_moments[i]
 
-            a[1][0][0] = neighbourdiff(Bx[0], i) - 1/10 * a[3][0][0]
+            a[1][0][0] = neighbourdiff(Bx[0], i)
+            if order > 2:
+                a[1][0][0] -= 1/10 * a[3][0][0]
     
     # 0th order
     for i in range(3):
         a = abc[i]
         Bx = B_moments[i]
 
-        a[0][0][0] = 1/2 * neighboursum(Bx[0], i) - 1/6 * a[2][0][0] - 1/70 * a[4][0][0]
+        a[0][0][0] = 1/2 * neighboursum(Bx[0], i)
+        if order > 1:
+            a[0][0][0] -= 1/6 * a[2][0][0]
+        if order > 3:
+            a[0][0][0] -= 1/70 * a[4][0][0]
 
     # Check constraint:
     #test = abc[0][1][0][0] + abc[1][1][0][0] + abc[2][1][0][0] + 1/10 * (abc[0][3][0][0] + abc[1][3][0][0] + abc[2][3][0][0])
@@ -381,55 +409,86 @@ def solve_all_coefficients(B_moments, order = 4):
     #    print("Something went wrong, sum (17) is " + str(test))
 
     # return np.pad(np.transpose(abc, (4, 5, 6, 0, 1, 2, 3)), [(0, 1), (0, 1), (0, 1), (0, 0), (0, 0), (0, 0), (0, 0)])
+
     return np.pad(abc, [(0, 0), (0, 0), (0, 0), (0, 0), (0, 1), (0, 1), (0, 1)])
 
-def center_value(B_moments, xyz, order=4):
+def center_value(B_moments, xyz):
     abc = solve_coefficients(B_moments, xyz, order)
     return [interpolate_x(abc[0], 0, 0, 0), interpolate_y(abc[1], 0, 0, 0), interpolate_z(abc[2], 0, 0, 0)]
 
-def ave_B(B_moments, xyz, order=4):
-    abc = solve_coefficients(B_moments, xyz, order)
+def ave_B(B_moments, xyz):
+    abc = solve_coefficients(B_moments, xyz)
     a = abc[0]
     b = abc[1]
     c = abc[2]
-    return (
-        a[0][0][0] - 3/8 * (a[2][0][0] + a[0][2][0] + a[0][0][2]) + 9/64 * (a[2][2][0] + a[2][0][2]) + 15/128 * a[4][0][0],
-        b[0][0][0] - 3/8 * (b[2][0][0] + b[0][2][0] + b[0][0][2]) + 9/64 * (b[2][2][0] + b[2][0][2]) + 15/128 * b[4][0][0],
-        c[0][0][0] - 3/8 * (c[2][0][0] + c[0][2][0] + c[0][0][2]) + 9/64 * (c[2][2][0] + c[2][0][2]) + 15/128 * c[4][0][0]
-    )
-
-def all_ave_Bs(B_moments, order=4):
-    abc = solve_all_coefficients(B_moments, order)
-    a = abc[0]
-    b = abc[1]
-    c = abc[2]
-    return np.transpose(np.array(
-        (
+    if order > 3:
+        return (
             a[0][0][0] - 3/8 * (a[2][0][0] + a[0][2][0] + a[0][0][2]) + 9/64 * (a[2][2][0] + a[2][0][2]) + 15/128 * a[4][0][0],
             b[0][0][0] - 3/8 * (b[2][0][0] + b[0][2][0] + b[0][0][2]) + 9/64 * (b[2][2][0] + b[2][0][2]) + 15/128 * b[4][0][0],
             c[0][0][0] - 3/8 * (c[2][0][0] + c[0][2][0] + c[0][0][2]) + 9/64 * (c[2][2][0] + c[2][0][2]) + 15/128 * c[4][0][0]
         )
-    ), [1, 2, 3, 0])
+    elif order > 1:
+        return (
+            a[0][0][0] - 3/8 * (a[2][0][0] + a[0][2][0] + a[0][0][2]) + 9/64 * (a[2][2][0] + a[2][0][2]),
+            b[0][0][0] - 3/8 * (b[2][0][0] + b[0][2][0] + b[0][0][2]) + 9/64 * (b[2][2][0] + b[2][0][2]),
+            c[0][0][0] - 3/8 * (c[2][0][0] + c[0][2][0] + c[0][0][2]) + 9/64 * (c[2][2][0] + c[2][0][2])
+        )
+    else:
+        return (
+            a[0][0][0],
+            b[0][0][0],
+            c[0][0][0]
+        )
 
-def center_values(B_moments, coords, order=4):
+def all_ave_Bs(B_moments):
+    abc = solve_all_coefficients(B_moments)
+    a = abc[0]
+    b = abc[1]
+    c = abc[2]
+    if order > 3:
+        return np.transpose(np.array(
+            (
+                a[0][0][0] - 3/8 * (a[2][0][0] + a[0][2][0] + a[0][0][2]) + 9/64 * (a[2][2][0] + a[2][0][2]) + 15/128 * a[4][0][0],
+                b[0][0][0] - 3/8 * (b[2][0][0] + b[0][2][0] + b[0][0][2]) + 9/64 * (b[2][2][0] + b[2][0][2]) + 15/128 * b[4][0][0],
+                c[0][0][0] - 3/8 * (c[2][0][0] + c[0][2][0] + c[0][0][2]) + 9/64 * (c[2][2][0] + c[2][0][2]) + 15/128 * c[4][0][0]
+            )
+        ), [1, 2, 3, 0])
+    elif order > 1:
+        return np.transpose(np.array(
+            (
+                a[0][0][0] - 3/8 * (a[2][0][0] + a[0][2][0] + a[0][0][2]) + 9/64 * (a[2][2][0] + a[2][0][2]),
+                b[0][0][0] - 3/8 * (b[2][0][0] + b[0][2][0] + b[0][0][2]) + 9/64 * (b[2][2][0] + b[2][0][2]),
+                c[0][0][0] - 3/8 * (c[2][0][0] + c[0][2][0] + c[0][0][2]) + 9/64 * (c[2][2][0] + c[2][0][2])
+            )
+        ), [1, 2, 3, 0])
+    else:
+        return np.transpose(np.array(
+                (
+                    a[0][0][0],
+                    b[0][0][0],
+                    c[0][0][0]
+                )
+            ), [1, 2, 3, 0])
+
+def center_values(B_moments, coords):
     # Looks scuffed but is faster
-    abc = np.transpose(np.transpose(solve_all_coefficients(B_moments, order), (4, 5, 6, 0, 1, 2, 3))[coords[:, 0], coords[:, 1], coords[:, 2]], (1, 2, 3, 4, 0))
+    abc = np.transpose(np.transpose(solve_all_coefficients(B_moments), (4, 5, 6, 0, 1, 2, 3))[coords[:, 0], coords[:, 1], coords[:, 2]], (1, 2, 3, 4, 0))
     return np.transpose(np.array([interpolate_x(abc[0], 0, 0, 0), interpolate_y(abc[1], 0, 0, 0), interpolate_z(abc[2], 0, 0, 0)]))
     #return all_center_values(B_moments, order)[coords[:, 0], coords[:, 1], coords[:, 2], :]
 
-def all_center_values(B_moments, order=4):
-    abc = solve_all_coefficients(B_moments, order)
+def all_center_values(B_moments):
+    abc = solve_all_coefficients(B_moments)
     return np.transpose(np.array([interpolate_x(abc[0], 0, 0, 0), interpolate_y(abc[1], 0, 0, 0), interpolate_z(abc[2], 0, 0, 0)]), (1, 2, 3, 0)) 
 
-def all_center_values_dbxdi(B_moments, order=4):
-    abc = solve_all_coefficients(B_moments, order)
+def all_center_values_dbxdi(B_moments):
+    abc = solve_all_coefficients(B_moments)
     return np.transpose(np.array([interpolate_dbxdx(abc[0], 0, 0, 0), interpolate_dbxdy(abc[0], 0, 0, 0), interpolate_dbxdz(abc[0], 0, 0, 0)]), (1, 2, 3, 0))
 
-def all_center_values_dbydi(B_moments, order=4):
-    abc = solve_all_coefficients(B_moments, order)
+def all_center_values_dbydi(B_moments):
+    abc = solve_all_coefficients(B_moments)
     return np.transpose(np.array([interpolate_dbydx(abc[1], 0, 0, 0), interpolate_dbydy(abc[1], 0, 0, 0), interpolate_dbydz(abc[1], 0, 0, 0)]), (1, 2, 3, 0))
 
-def all_center_values_dbzdi(B_moments, order=4):
-    abc = solve_all_coefficients(B_moments, order)
+def all_center_values_dbzdi(B_moments):
+    abc = solve_all_coefficients(B_moments)
     return np.transpose(np.array([interpolate_dbzdx(abc[2], 0, 0, 0), interpolate_dbzdy(abc[2], 0, 0, 0), interpolate_dbzdz(abc[2], 0, 0, 0)]), (1, 2, 3, 0))
 
