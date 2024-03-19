de1f4757e74cbd5c87019f8f6c2ad375e5a8b068
JonasSuni <jonas.suni@helsinki.fi>
2024-03-19 11:53:21 +0200

    Provide points list instead of start and end points

--- scripts/cutthrough_timeseries.py
+++ scripts/cutthrough_timeseries.py
@@ -11,25 +11,26 @@ def jplots(
     var,
     fnr0,
     fnr1,
-    x0,
-    y0,
-    z0,
-    x1,
-    y1,
-    z1,
-    dr,
+    # x0,
+    # y0,
+    # z0,
+    # x1,
+    # y1,
+    # z1,
+    # dr,
+    pointfile,
     bulkpath,
     bulkprefix,
     outputname,
-    outputdir="./",
-    intpol=False,
-    filt=-1,
-    op="pass",
-    cmap="viridis",
+    outputdir,
+    intpol,
+    filt,
+    op,
+    cmap,
 ):
 
-    dr *= 1000
-    dr /= r_e
+    # dr *= 1000
+    # dr /= r_e
 
     fnr_arr = np.arange(fnr0, fnr1 + 0.1, 1, dtype=int)
     t_arr = np.zeros_like(fnr_arr).astype(float)
@@ -37,23 +38,30 @@ def jplots(
     if bulkpath[-1] != "/":
         bulkpath += "/"
 
-    npoints = int(np.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2 + (z1 - z0) ** 2) / dr) + 1
+    # npoints = int(np.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2 + (z1 - z0) ** 2) / dr) + 1
 
-    xlist = np.linspace(x0, x1, npoints, dtype=float)
-    ylist = np.linspace(y0, y1, npoints, dtype=float)
-    zlist = np.linspace(z0, z1, npoints, dtype=float)
+    # xlist = np.linspace(x0, x1, npoints, dtype=float)
+    # ylist = np.linspace(y0, y1, npoints, dtype=float)
+    # zlist = np.linspace(z0, z1, npoints, dtype=float)
 
-    if intpol:
-        coords = r_e * np.array([xlist, ylist, zlist]).T
-    else:
+    # if intpol:
+    #     coords = r_e * np.array([xlist, ylist, zlist]).T
+    # else:
+    #     fobj = pt.vlsvfile.VlsvReader(
+    #         bulkpath + bulkprefix + ".{}.vlsv".format(str(fnr0).zfill(7))
+    #     )
+    #     cellids = [
+    #         int(fobj.get_cellid([xlist[idx] * r_e, ylist[idx] * r_e, 0]))
+    #         for idx in range(xlist.size)
+    #     ]
+    # point_list = np.arange(xlist.size)
+    coords = np.loadtxt(pointfile) * r_e
+    point_list = np.arange(len(coords))
+    if not intpol:
         fobj = pt.vlsvfile.VlsvReader(
             bulkpath + bulkprefix + ".{}.vlsv".format(str(fnr0).zfill(7))
         )
-        cellids = [
-            int(fobj.get_cellid([xlist[idx] * r_e, ylist[idx] * r_e, 0]))
-            for idx in range(xlist.size)
-        ]
-    point_list = np.arange(xlist.size)
+        cellids = [int(fobj.get_cellid(coord)) for coord in coords]
 
     data_arr = np.zeros((fnr_arr.size, point_list.size), dtype=float)
 
@@ -120,13 +128,14 @@ if __name__ == "__main__":
         print("var: Variable to plot")
         print("fnr0: First file number to plot")
         print("fnr1: Last file number to plot")
-        print("x0: Cut-through starting point x coordinate [Re]")
-        print("y0: Cut-through starting point y coordinate [Re]")
-        print("z0: Cut-through starting point z coordinate [Re]")
-        print("x1: Cut-through ending point x coordinate [Re]")
-        print("y1: Cut-through ending point y coordinate [Re]")
-        print("z1: Cut-through ending point z coordinate [Re]")
-        print("dr: Distance between cut-through sample points (km)")
+        # print("x0: Cut-through starting point x coordinate [Re]")
+        # print("y0: Cut-through starting point y coordinate [Re]")
+        # print("z0: Cut-through starting point z coordinate [Re]")
+        # print("x1: Cut-through ending point x coordinate [Re]")
+        # print("y1: Cut-through ending point y coordinate [Re]")
+        # print("z1: Cut-through ending point z coordinate [Re]")
+        # print("dr: Distance between cut-through sample points (km)")
+        print("pointfile: File containing a list of coordinates, in Re")
         print("bulkpath: Path to bulk files")
         print("bulkprefix: Starting string of bulk file name (e.g. bulk, bulk1, bulk5)")
         print("outputname: Name of output file")
@@ -146,13 +155,14 @@ if __name__ == "__main__":
         var,
         fnr0,
         fnr1,
-        x0,
-        y0,
-        z0,
-        x1,
-        y1,
-        z1,
-        dr,
+        # x0,
+        # y0,
+        # z0,
+        # x1,
+        # y1,
+        # z1,
+        # dr,
+        pointfile,
         bulkpath,
         bulkprefix,
         outputname,
@@ -167,13 +177,14 @@ if __name__ == "__main__":
         var,
         int(fnr0),
         int(fnr1),
-        float(x0),
-        float(y0),
-        float(z0),
-        float(x1),
-        float(y1),
-        float(z1),
-        float(dr),
+        # float(x0),
+        # float(y0),
+        # float(z0),
+        # float(x1),
+        # float(y1),
+        # float(z1),
+        # float(dr),
+        pointfile,
         bulkpath,
         bulkprefix,
         outputname,
