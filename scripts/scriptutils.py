78557df00e1242d67667bc061f214542192a9267
Horaites Konstantinos <horakons@turso01.cs.helsinki.fi>
2024-04-22 14:54:53 +0300

    Cleaned up scriptutils.py Most importantly, now scriptutils.py only contains functions that are used in other .py files in scripts/ folder

--- scripts/scriptutils.py
+++ scripts/scriptutils.py
@@ -4,35 +4,6 @@ import os
 import numpy as np
 from functools import wraps
 from time import time
-import hashlib
-import re
-
-global mu_0
-mu_0 = 4e-7 * np.pi
-
-try:
-    from pyspedas.utilities.time_string import *
-except:
-    from pyspedas import time_string, time_double
-    
-
-'''
- show() creates uses display to display a plot created by tell(),
- which is saved in a default location
- the input, plt, is assumed to be a matplotlib plot
-
-    ex. (on Turso login node, won't work on a compute note)
- import matplotlib.pyplot as plt
- from utils import *
- plt.plot([1,2,3])
- tell(plt)
- show()
-
-Note that show (and tell?) won't work in an interactive session.
-
-Another (more useful) option is to just type 'show' into the command line on turso
-
-''' 
 
 def get_bulklocation(run):
     # load data
@@ -93,50 +64,8 @@ def get_filename(run, fileIndex):
     return filename
 
 
-def timeseries(run, var_list, start, stop, filestem = None):     #, step?
-    '''
-        run: name of the Vlasiator run. ex.  'EGL', 'FHA'
-        var_list: a list of variable names (strings)
-            specify operators using 'variable.operator'
-        start: first timestep
-        stop: last timestep
-        filestem: set this keyword to the filename, omitting the end 'XXXXXXX.vlsv'
-    '''
-    import pytools as pt
-    if type(var_list) == str:
-        var_list = [var_list]
-    dct = {}
-    for var in var_list:
-        var_timeseries = []
-        for fileIndex in range(start, stop+1):
-            if filestem is None:
-                filename = get_vlsvfile_fullpath(run, fileIndex)
-            else:
-                filename = filestem + str(fileIndex).zfill(7) + '.vlsv'
-            f = pt.vlsvfile.VlsvReader(filename)
-            if '.' in var:
-                l = var.split('.')
-                vartemp = l[0]; operator = l[1]
-                var_timeseries.append(f.read_variable(vartemp, operator = operator))
-            else:
-                var_timeseries.append(f.read_variable(var))
-        var_timeseries = np.array(var_timeseries)
-        dct[var] = var_timeseries
-    return dct
-
-
 def get_vlsvfile_fullpath(run, fileIndex):
     return get_bulklocation(run) + get_filename(run, fileIndex)
-
-
-def tell(plt):
-   tmp_dir = '/wrk-vakka/users/horakons/temp/'
-   plt.savefig(temp_dir+'plot.png', dpi=300, bbox_inches='tight')
-
-
-def show():
-   tmp_dir = '/wrk-vakka/users/horakons/temp/'
-   os.system('display ' + tmp_dir + 'plot.png')
 
 
 def cartesian_to_spherical(x, y, z):
@@ -169,53 +98,11 @@ def spherical_to_cartesian(r, theta, phi):
     return x, y, z
 
 
-def cartesian_to_spherical_vector(vx, vy, vz, x, y, z):
-    '''
-    Convert cartesian vector(s) with coordinates (vx, vy, vz)
-    at the position(s) theta, phi (note: position r does not affect the vector transformation)
-    to spherical coordinates (v_r, v_theta, v_phi)
-
-    dimensions of vx, vy, vz, x, y, z arrays must either match or be a single number
-    '''
-    r, theta, phi = cartesian_to_spherical(x, y, z)
-    v_r =     vx * np.sin(theta) * np.cos(phi) + vy * np.sin(theta) * np.sin(phi) + vz * np.cos(theta)
-    v_theta = vx * np.cos(theta) * np.cos(phi) + vy * np.cos(theta) * np.sin(phi) - vz * np.sin(theta)
-    v_phi =  -vx * np.sin(phi) + vy * np.cos(phi)
-    return v_r, v_theta, v_phi
-
-
 def mkdir_path(path):
     filedir_list = path.split('/')
     filedir = path[:-len(filedir_list[-1])]
     if not(os.path.exists(filedir)):
          os.system('mkdir -p {}'.format(filedir))
-
-
-def numcurl3d(inputarray, CELLSIZE_XYZ):
-    # Assumes input array is of format [nx,ny,nz,3]
-    # CELLSIZE_XYZ is 3-element array or list of grid spacings
-    jac = numjacobian3d(inputarray, CELLSIZE_XYZ)
-    # Output array is of format [nx,ny,nz,3]
-    curl = np.zeros(inputarray.shape)
-    curl[:,:,:,0] = jac[:,:,:,2,1]-jac[:,:,:,1,2]
-    curl[:,:,:,1] = jac[:,:,:,0,2]-jac[:,:,:,2,0]
-    curl[:,:,:,2] = jac[:,:,:,1,0]-jac[:,:,:,0,1]
-    return curl
-
-
-def numjacobian3d(inputarray, CELLSIZE_XYZ):
-    # Assumes input array is of format [nx,ny,nz,3]
-    # CELLSIZE_XYZ is 3-element array or list of grid spacings
-    nx,ny,nz = inputarray[:,:,:,0].shape
-    jac = np.zeros([nx,ny,nz,3,3])
-    jac[:,:,:,0,0], jac[:,:,:,0,1], jac[:,:,:,0,2] = np.gradient(inputarray[:,:,:,0], CELLSIZE_XYZ[0])
-    jac[:,:,:,1,0], jac[:,:,:,1,1], jac[:,:,:,1,2] = np.gradient(inputarray[:,:,:,1], CELLSIZE_XYZ[1])
-    jac[:,:,:,2,0], jac[:,:,:,2,1], jac[:,:,:,2,2] = np.gradient(inputarray[:,:,:,2], CELLSIZE_XYZ[2])
-    # Output array is of format [nx,ny,nz,3,3]
-    #  :,:,component, derivativedirection
-    # so for example: dAx/dx = :,:,:,0,0
-    #                 DAy/dz = :,:,:,1,2
-    return jac
 
 
 def save(file,**kwargs):
@@ -273,88 +160,4 @@ def timer(my_func, *args, **kwargs):
             return output
     return wrapTheFunction
 
-
-def sidecar(my_func, *args, **kwargs):
-    '''
-     sidecar is meant to be used as a python decorator.
-     Use @sidecar before the function definition.
-     When the decorated function is called, the the local sidecar/
-     directory will be searched for a .pck (Pickle) file that
-     contains the results of the function call for the given inputs
-     If no such file is found, a new .pck file is created upon completion
-     Basically behaves like a cache in permanent memory.
-     
-      #Example 1:
-    @sidecar
-    def sq(x, keyw='test'):
-        print(keyw)
-        return(x**2)
-    
-    sq(3)
-
-      #Example 2:
-    len = sidecar(len)
-    len([1,2,'asdf',None]) 
-    '''
-    @wraps(my_func)
-    def wrapTheFunction(*args, **kwargs):
-        # Encode the signature as a string, then save it as a hash
-        signature_string = "{}_{}_{}".format(my_func.__name__,args, str(sorted(kwargs))+str([kwargs[x] for x in sorted(kwargs)]))
-        signature_string = re.sub(r'<.*?at.*?>', '', signature_string)   #get rid of references to specific memory addresses, that might change btw. calls
-        signature_hash = hashlib.md5('{}'.format(signature_string).encode()).hexdigest()
-        filename = "sidecar/{}/{}.pck".format(my_func.__name__,signature_hash) 
-        mkdir_path(filename)  # make a sidecar/directory if it doesn't exist
-        if os.path.exists(filename):
-            print('restoring sidecar file for {}(): {}'.format(my_func.__name__, filename))
-            tmp = restore(filename)
-            output = tmp['output']
-        else:
-            print('saving sidecar file for {}(): {}'.format(my_func.__name__, filename) )       # note: if you try to pickle an I/O buffer, an exception will be thrown
-            output = my_func(*args, **kwargs)
-            save(filename, output=output)
-        return output
-    return wrapTheFunction
-
-
-
-
-def save(file,**kwargs):
-    """
-    Save the value of some data in a file.
-    Usage: 
-    a = 3; bvar = 'string'; test = [True, '300', 3.14]
-    save('misdatos.pickle',a=a,b=bvar,test=test)
-    """
-    import pickle
-    f=open(file,"wb")
-    pickle.dump(kwargs,f,protocol=2)
-    f.close
-
-
-def restore(file):
-    """
-    Read data saved with save function.
-    Usage: datos = restore('misdatos.pickle')    #datos is a dict with the saved variables
-           print(datos['test'])                  #e.g.
-    """
-    import pickle
-    f=open(file,"rb")
-    result = pickle.load(f)
-    f.close
-    return result
-
-
-
-def t_string(t, epoch = False):
-    if epoch:
-        return time_string(t/1000 -  719528. * 24.* 3600.)
-    else:
-        return time_string(t)
-
-
-def t_double(date, epoch = False):
-    if epoch:
-        return (time_float(date) + 719528. * 24.* 3600.) * 1000
-    else:
-        return time_float(date)
 
