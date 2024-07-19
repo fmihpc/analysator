f3b8d74e1ba2b9de464e204bb5c3af12679e6a16
Yann Kempf <yann.kempf@helsinki.fi>
2024-07-19 17:29:56 +0300

    Fix get_cmap API in other plotting functions too.

--- pyPlots/plot_isosurface.py
+++ pyPlots/plot_isosurface.py
@@ -156,7 +156,10 @@ def plot_isosurface(filename=None,
         colormap="hot_desaturated"
         if color_op!=None:
             colormap="bwr"
-    cmapuse=matplotlib.cm.get_cmap(name=colormap)
+    if Version(matplotlib.__version__) < Version("3.5.0"):
+        cmapuse=matplotlib.cm.get_cmap(name=colormap)
+    else:
+        cmapuse=matplotlib.colormaps.get_cmap(colormap)
 
     fontsize=8*scale # Most text
     fontsize2=10*scale # Time title
@@ -1018,8 +1021,11 @@ def plot_neutral_sheet(filename=None,
         colormap="hot_desaturated"
         if operator is not None and operator in 'xyz':
             colormap="bwr"
-    cmapuse=matplotlib.cm.get_cmap(name=colormap)
-
+    if Version(matplotlib.__version__) < Version("3.5.0"):
+        cmapuse=matplotlib.cm.get_cmap(name=colormap)
+    else:
+        cmapuse=matplotlib.colormaps.get_cmap(colormap)
+    
     fontsize=8*scale # Most text
     fontsize2=10*scale # Time title
     fontsize3=8*scale # Colour bar ticks and title
