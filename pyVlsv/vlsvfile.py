ddb8ad219c966ee2349e611f195fd52d340f000f
Markku Alho <markku.alho@helsinki.fi>
2024-10-04 15:37:27 +0300

    Move to a reduction_sidecar file for auxiliary datareducers

--- pyVlsv/vlsvfile.py
+++ pyVlsv/vlsvfile.py
@@ -40,4 +40,5 @@ from vlsvwriter import VlsvWriter
 from vlasiatorreader import VlasiatorReader
 
 from vlsvparticles import VlsvParticles
-from reduction import GGT, GTG, MDD, MGA, J, LMN, LMN_xoline_distance, MDD_dimensionality
+import reduction
+import reduction_sidecar
