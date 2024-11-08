0121568c465b6e54fc044554edd81bfd9eaef0a6
Alho Markku J <markku.alho@helsinki.fi>
2024-11-08 16:37:16 +0200

    Actually fix filename dosctring?

--- miscellaneous/filenames.py
+++ miscellaneous/filenames.py
@@ -31,7 +31,7 @@ import os
 def get_sorted_file_names(name="*.vlsv"):
    ''' Gets the file names in the current directory and sorts them.
 
-         :param name:            Name of the file(s), for example "*.vlsv"
+         :param name:            Name of the file(s), for example `"*.vlsv"`
          :returns: a list of file names in sorted order
    '''
    fileNames=glob.glob(name)
