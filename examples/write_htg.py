71d8b5beb5f72b91c05438614689961b3b9744e6
Alho Markku J <markku.alho@helsinki.fi>
2025-01-17 14:44:41 +0200

    Refreshed example

--- examples/write_htg.py
+++ examples/write_htg.py
@@ -2,15 +2,17 @@ import vtk
 import pytools as pt
 
 # This initializes a hypertreegrid from the given reader.
-reader = pt.vlsvfile.VlsvReader("../../bulk.0002189.vlsv")
-htg = pt.vlsvfile.vtkVlsvHyperTreeGrid(reader)
+reader = VlsvVtkReader()
+reader.SetFileName("../../bulk.0002189.vlsv")
 
 # These functions grab one SpatialGrid variable and map that to 
 # the hypertreegrid. Variable vector sizes of 1,2,3,4,9 supported.
-htg.addArrayFromVlsv("vg_b_vol")
-htg.addArrayFromVlsv("vg_beta_star")
+reader.addArrayFromVlsv("vg_b_vol")
+reader.addArrayFromVlsv("vg_beta_star")
+
+reader.Update()
 
 writer = vtk.vtkXMLHyperTreeGridWriter()
 writer.SetFileName("output_EGE.htg")
-writer.SetInputData(htg)
+writer.SetInputData(reader.GetOutputDataObject(0))
 writer.Write()
