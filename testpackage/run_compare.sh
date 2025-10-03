fd8cfbffd1938183205f1994f135e287d2c3cff9
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-10-03 16:57:57 +0300

    testings

--- testpackage/run_compare.sh
+++ testpackage/run_compare.sh
@@ -4,6 +4,7 @@
 
 folder_1="/wrk-vakka/turso/group/spacephysics/CI_analysator/analysator_testpackage/verification_sets/380989e7fa7a331fb90c7ac6c496ebec6397dec9/"
 folder_2="${PWD}/produced_plots/"
+echo $(ls $(folder_2))
 output=$((python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2}) 2>&1)
 
 
