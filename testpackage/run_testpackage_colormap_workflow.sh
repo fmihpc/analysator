051b6e1d56e3f29ad2db9fa046d1b5d0f6360335
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-10-03 16:30:33 +0300

    added plot production (hopefully, not sure if it might explode)

---
+++ testpackage/run_testpackage_colormap_workflow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -t 00:30:00
+#SBATCH -J analysator_testpackage
+#SBATCH -p short
+#SBATCH -n 1
+#SBATCH --array=0-20
+#SBATCH --no-requeue
+#SBATCH --mem-per-cpu=16000
+
+
+#THIS SHOULD ONLY BE USED FOR GITHUB WORKFLOW TESTS
+jobcount=$(( $SLURM_ARRAY_TASK_MAX - $SLURM_ARRAY_TASK_MIN + 1 )) 
+index=$(( $SLURM_ARRAY_TASK_ID - $SLURM_ARRAY_TASK_MIN ))
+
+hostname
+source CI_env/bin/activate
+mkdir $PWD/produced_plots
+
+export PTNONINTERACTIVE=1
+export PTOUTPUTDIR=$PWD/produced_plots/
+
+python testpackage_colormap.py $jobcount $index
+echo Job $SLURM_ARRAY_TASK_ID complete.
