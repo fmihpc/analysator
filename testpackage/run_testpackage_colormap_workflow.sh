4ddfd3dd0843ec32667e8b5149df708ddb3f3ece
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-10-06 10:15:51 +0300

    even more workflow debugging

--- testpackage/run_testpackage_colormap_workflow.sh
+++ testpackage/run_testpackage_colormap_workflow.sh
@@ -3,7 +3,7 @@
 #SBATCH -J analysator_testpackage
 #SBATCH -p short
 #SBATCH -n 1
-#SBATCH --array=0-5
+#SBATCH --array=1
 #SBATCH --no-requeue
 #SBATCH --mem-per-cpu=16000
 
@@ -11,6 +11,8 @@
 #THIS SHOULD ONLY BE USED FOR GITHUB WORKFLOW TESTS
 jobcount=$(( $SLURM_ARRAY_TASK_MAX - $SLURM_ARRAY_TASK_MIN + 1 )) 
 index=$(( $SLURM_ARRAY_TASK_ID - $SLURM_ARRAY_TASK_MIN ))
+
+echo $PWD
 
 hostname
 source CI_env/bin/activate
