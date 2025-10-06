1e22599ac98cc76f7792f2ecd7de412c671f5978
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-10-06 10:59:15 +0300

    test

--- testpackage/run_testpackage_colormap_workflow.sh
+++ testpackage/run_testpackage_colormap_workflow.sh
@@ -15,8 +15,14 @@ index=$(( $SLURM_ARRAY_TASK_ID - $SLURM_ARRAY_TASK_MIN ))
 echo $PWD
 
 hostname
+
+
+module purge            
+export PATH=/wrk-vakka/group/spacephysics/proj/appl/tex-basic/texlive/2023/bin/x86_64-linux:$PATH
+module load Python/3.10.4-GCCcore-11.3.0
+module load ImageMagick/7.1.0-37-GCCcore-11.3.0
+
 source CI_env/bin/activate
-echo $PYTHONPATH
 mkdir -p $PWD/produced_plots
 
 export PTNONINTERACTIVE=1
