9c6d3ae0d207eb9a42c1f01d0e3f5d00d7fad6c4
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-10-07 15:21:35 +0300

    test

--- testpackage/run_compare.sh
+++ testpackage/run_compare.sh
@@ -5,7 +5,7 @@
 #verification set is from: 6.10.2025 
 folder_1="/wrk-vakka/turso/group/spacephysics/CI_analysator/analysator_testpackage/verification_sets/380989e7fa7a331fb90c7ac6c496ebec6397dec9/"
 folder_2="${PWD}/produced_plots/"
-output=$((python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2}) 2>&1)
+output=$((python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2}) 2>&1 || exit 1)
 
 
 if [[ $output == "" ]]; then
