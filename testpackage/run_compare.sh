c34590a66d853d6285f8e094bbf754d0825c459d
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-10-07 15:38:40 +0300

    sigh

--- testpackage/run_compare.sh
+++ testpackage/run_compare.sh
@@ -5,13 +5,15 @@
 #verification set is from: 6.10.2025 
 folder_1="/wrk-vakka/turso/group/spacephysics/CI_analysator/analysator_testpackage/verification_sets/380989e7fa7a331fb90c7ac6c496ebec6397dec9/"
 folder_2="${PWD}/produced_plots/"
-output=$((python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2}) 2>&1 || exit 1)
+python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2}
+
+#output=$((python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2}) 2>&1)
 
 
-if [[ $output == "" ]]; then
-    echo "No differences found"
-    exit 0
-else
-    echo "$output"
-fi
+#if [[ $output == "" ]]; then
+#    echo "No differences found"
+#    exit 0
+#else
+#    echo "$output"
+#fi
 
