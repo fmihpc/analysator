fb78ba7ea7ff4a92451e3b05c8aa02a78d8b94ea
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-10-31 11:19:09 +0200

    Minor fixes and better system_call function

--- testpackage/create_env.py
+++ testpackage/create_env.py
@@ -4,6 +4,27 @@ import subprocess
 from sys import version_info as python_version_info
 
 
+
+
+def system_call(cmd,live_output=False):
+    with subprocess.Popen(cmd.split(" "),stdout=subprocess.PIPE,stderr=subprocess.PIPE) as proc:
+        if live_output:
+            for line in proc.stdout:
+                print(str(line,'utf-8').rstrip('\n')) #Note that for example pip's progress bar is not displayed
+
+    out,err = proc.communicate()
+
+    #If errors, raise an exception
+    if proc.returncode!=0:
+        err = str(err,'utf-8')
+        raise RuntimeError(err)
+
+    out = str(out,'utf-8').rstrip('\n')
+
+    
+
+    return out
+
 def create_venv(path,install_analysator=True,editable=False):
     virt_env= venv.EnvBuilder(with_pip=True,upgrade_deps=True)
     context=virt_env.ensure_directories(path)
@@ -17,54 +38,45 @@ def create_venv(path,install_analysator=True,editable=False):
     virt_env.create_configuration(context)
     virt_env.setup_scripts(context)
     if install_analysator:
-        os.system(f'{path}/bin/pip install {'--editable' if editable else ''} ../')
+        system_call(f'{path}/bin/pip install {'--editable' if editable else ''} ../',live_output=True)
     print(f'Virtual environment created at {path}')
-    return 0
-
-def system_call(cmd,wait=False):
-    proc = subprocess.Popen(cmd.split(" "),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-
-    out,err = proc.communicate()
-
-    #If errors, raise an exception
-    if err:
-        err = str(err,'utf-8')
-        raise RuntimeError(err)
-
-    out = str(out,'utf-8').rstrip('\n')
-    if wait:
-        proc.wait()
-    return out
-
+    return None
 
 if __name__ == "__main__":
-    create_venv_CI = True
-    create_venv_local = False
+    
+    create_venv_CI = False          #Create a virtual environment for CI in wrk-vakka
+    create_venv_local = True        #Create a virtual environment in the current folder (should be in testpackage folder)
+
 
     ci_path="/wrk-vakka/turso/group/spacephysics/CI_analysator/analysator_testpackage"
 
     if not 'venv_testpackage' in os.listdir('.') and create_venv_local:
         print('venv_testpackage not found, creating virtual environment')
-        os.system("mkdir venv_testpackage")  #Is this necessary?
         create_venv('venv_testpackage',editable=True)
+    else:
+        print('venv_testpackage found, not creating virtual environment')
 
+
+
+
+    #Create a virtual environment for CI in wrk-vakka
     if create_venv_CI:
         print('Creating virtual environment for CI')
         import time
-        time=time.strftime("%c",time.gmtime()).replace(" ","_").replace(":","-")
+        gmtime_now=time.strftime("%c",time.gmtime()).replace(" ","_").replace(":","-")
 
         git_branch = system_call('git rev-parse --abbrev-ref HEAD')
 
 
         #Are we on master branch?
-        if git_branch!='master':
+        if git_branch not in ('master','main') :
             user_input=input('::warning not in master branch, are you sure you want to continue? y/n\n')
             if user_input.capitalize() != 'Y':
                 quit()
 
-        path=f"{ci_path}/venv_testpackage-{time}-{git_branch}"
+        path=f"{ci_path}/venv_testpackage-{gmtime_now}-{git_branch}"
 
         create_venv(path,install_analysator=True,editable=False) #Editable should be false (default) for CI!
-        
+
         #Make group writable
-        print(system_call(f'chmod g+w {path}'))
+        print(system_call(f'chmod -R g+w {path}'))
