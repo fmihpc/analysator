400a637c801645f5a8ecfabb34f9e5cca487b780
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-10-31 13:53:30 +0200

    deleted pyvenv.sh as it is now made by create_env script if needed

--- testpackage/pyvenv.sh
+++
@@ -1,8 +0,0 @@
-module purge
-export PATH=/wrk-vakka/group/spacephysics/proj/appl/tex-basic/texlive/2023/bin/x86_64-linux:$PATH
-module load Python/3.10.4-GCCcore-11.3.0
-module load ImageMagick/7.1.0-37-GCCcore-11.3.0
-module list
-#source /wrk-vakka/group/spacephysics/proj/CI_analysator/analysator_testpackage/python3.10.4/bin/activate
-source ./venv_testpackage/bin/activate
-
