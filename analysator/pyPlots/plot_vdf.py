d8fe9ed1d980a3343fe2da726e51fea57707ea31
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-10-31 14:56:32 +0200

    Changed behaviour of fixfigsize and renamed it figsize to fix figsize

--- analysator/pyPlots/plot_vdf.py
+++ analysator/pyPlots/plot_vdf.py
@@ -427,7 +427,7 @@ def plot_vdf(filename=None,
              biglabel=None, biglabloc=None,
              noxlabels=None, noylabels=None,
              axes=None, cbaxes=None,
-             contours=None,fixfigsize=False
+             contours=None,figsize=None
              ):
 
     ''' Plots a coloured 2D plot of a VDF (a slice of given thickness or fully projected) with axes and a colour bar.
@@ -521,8 +521,7 @@ def plot_vdf(filename=None,
     :kword scale_cb:    Colour bar text additional scale factor (default=5.0)
     :kword scale_label: Big label text additional scale factor (default=12.0)
     :kword thick:       line and axis thickness, default=1.0
-    :kword fixfigsize:  Used for fixing the size of the output image for testpackage or for comparing images with ImageMagick compare.
-                        This should not be used with noborder, as it may undo it.
+    :kword figsize:     Set figure size, default=None which will use [4.0,3.15*ratio]
 
     :returns:           Outputs an image to a file or to the screen.
 
@@ -1135,25 +1134,24 @@ def plot_vdf(filename=None,
         # Define figure size
         ratio = (yvalsrange[1]-yvalsrange[0])/(xvalsrange[1]-xvalsrange[0])
         # default for square figure is figsize=[4.0,3.15]
-        figsize = [4.0,3.15*ratio]
 
         # Plot the slice
         [XmeshXY,YmeshXY] = np.meshgrid(edgesX/velUnit,edgesY/velUnit) # Generates the mesh to map the data to
 
         if axes is None:
-            if fixfigsize is not None:
+            if figsize is not None:
                 #Matplotlib jank, it noborder changed the imagesize even though you'd think insetting would work
                 #it did fix the size if inset was something weird like [0.5,0.5,0.5,0.5] *shrug*
 
                 if noborder: 
-                    logging.warning("noborder and fixfigsize enabled, this may undo effects of fixfigsize")
-                fig = plt.figure(figsize=[5,4],dpi=300)
+                    logging.warning("noborder and figsize enabled, this may undo effects of figsize")
+                fig = plt.figure(figsize=figsize,dpi=300)
                 axm=plt.gca()
                 axm.axis('off')
                 ax1 = axm.inset_axes([0.05,0.05,0.9,0.9]) 
             else:
                 # Create 300 dpi image of suitable size
-                fig = plt.figure(figsize=figsize,dpi=300)
+                fig = plt.figure(figsize=[4.0,3.15*ratio] ,dpi=300)
                 ax1 = plt.gca() # get current axes
 
         else:
