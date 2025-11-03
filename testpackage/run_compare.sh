28607dedcf23e1e1d5768db8f6fa3d527f6a6e3d
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-11-03 11:00:00 +0200

    oops comment removed

--- testpackage/run_compare.sh
+++ testpackage/run_compare.sh
@@ -6,4 +6,4 @@ verf_loc="/wrk-vakka/turso/group/spacephysics/CI_analysator/analysator_testpacka
 #gets latest verfication set (based on modification date -> grep directories only -> take firstline -> get last word)
 folder_1="$verf_loc/$(ls -lth $verf_loc | grep ^d | head -n1 | grep -Po '\w+$')/$1/" 
 folder_2="${PWD}/produced_plots/$1/"
-#python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2} && echo "No differences found in produced images"
+python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2} && echo "No differences found in produced images"
