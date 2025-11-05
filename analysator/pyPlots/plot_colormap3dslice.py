ebc85946148c10cb8c928d35491e7f1fac258783
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-11-05 09:05:46 +0200

    changed error to warning

--- analysator/pyPlots/plot_colormap3dslice.py
+++ analysator/pyPlots/plot_colormap3dslice.py
@@ -1277,7 +1277,7 @@ def plot_colormap3dslice(filename=None,
                 lines = np.vstack((vlines, hlines))
                 ax1.add_collection(LineCollection(lines, lw=fluxropelinewidth, colors=fluxropecolour, linestyle=fluxropelinestyle))
         else:
-            raise ValueError('parameter "fluxrope" passed to plot_colormap3dslice but no vg_fluxrope variable found in data')
+            logging.warning('Parameter "fluxrope" passed to plot_colormap3dslice but no vg_fluxrope variable found in data')
     # add AMR contours
     if amr is not None:
         if np.isscalar(amr):
