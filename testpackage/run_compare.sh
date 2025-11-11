ce1b17adca8359bec1c90b92d863c8717b0facc9
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-11-11 16:26:23 +0200

    run compare now runs correctly if called without commandline args, which will result in comparing all

--- testpackage/run_compare.sh
+++ testpackage/run_compare.sh
@@ -7,11 +7,22 @@ if echo $@ | grep -q -P "\spass$|\spass\s"; then
    exit 0
 fi
 
+check=true
+
 for i in $@
 do
-    echo "Testing for $i"
+    check=false
+    echo "Comparing for $i"
     #gets latest verfication set (based on modification date -> grep directories only -> take firstline -> get last word)
-    folder_1="$verf_loc/$(ls -lth $verf_loc | grep ^d | head -n1 | grep -Po '\w+$')/$i/" 
+    folder_1="$verf_loc/$(ls -lth $verf_loc | grep ^d | head -n1 | grep -Po '\w+$')/testpackage_run/$i/" 
     folder_2="${PWD}/produced_plots/testpackage_run/$i/"
     python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2} && echo "No differences found in produced images"
 done
+
+if $check;
+then
+    echo "Comparing all"
+    folder_1="$verf_loc/$(ls -lth $verf_loc | grep ^d | head -n1 | grep -Po '\w+$')/testpackage_run/" 
+    folder_2="${PWD}/produced_plots/testpackage_run/"
+    python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2} && echo "No differences found in produced images"
+fi
