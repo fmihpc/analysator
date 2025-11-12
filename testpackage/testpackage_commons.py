e49b175d902d8523ebd668798ada562a2cc623a3
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-11-12 12:30:29 +0200

    finished renaming regularcalls to restartcalls

--- testpackage/testpackage_commons.py
+++ testpackage/testpackage_commons.py
@@ -828,11 +828,11 @@ manualcalls=[
 #keys: v5bulk,v5restart,bulk,restart,v5multipop,multipop
 
 # For handier debugging, uncomment these to overwrite call lists and include only relevant calls
-# regularcalls = []
+# restartcalls = []
 # nonrestartcalls = ["pt.plot.plot_colormap3dslice(filename=fileLocation+bulkname, run=verifydir+REPLACEINDEX, expression=expr_cav_cust, pass_times=3, pass_vars=['rho','B','beta'],lin=1,colormap='bwr',usesci=0)","pt.plot.plot_colormap3dslice(filename=fileLocation+bulkname, run=verifydir+REPLACEINDEX, expression=expr_cav_cust, pass_times=3,lin=1,colormap='bwr',usesci=0, boxre=[0,30,-15,15])",
 # ]
 # multipopcalls = []
-# v5regularcalls = []
+# v5restartcalls = []
 # v5nonrestartcalls = []
 # v5multipopcalls = []
 
@@ -934,7 +934,7 @@ for i,run in enumerate(runs):
             callindex += 1
 
     for j,func in enumerate(functions):
-        calls_in=v5restartcalls if vlasiator5 else regularcalls
+        calls_in=v5restartcalls if vlasiator5 else restartcalls
         for call in calls_in:
             call=call_replace(call,func,skipped_args)
             if not call:
