85e7f4399957a459fca095a37041586840f807f3
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-11-24 10:23:44 +0200

    moved cbfmtsci away from variable to plot.py

--- analysator/pyPlots/plot.py
+++ analysator/pyPlots/plot.py
@@ -51,6 +51,7 @@ from plot_threeslice import plot_threeslice
 from plot_ionosphere import plot_ionosphere
 import platform
 from packaging.version import Version
+from numbers import Number
 
 try:
     from plot_isosurface import plot_isosurface, plot_neutral_sheet
@@ -263,3 +264,161 @@ def textbfstring(string):
             return r'\textbf{'+string+'}'
     # LaTex output off
     return string
+
+def get_scaled_units( vscale=None, env='EarthSpace', manualDict=None,variable_info=None):
+    ''' Return scaling metadata
+
+        :param env:          A string to choose the scaling dictionary [default: EarthSpace]
+        :param manualDict:   a dictionary of {units : {scalingparams}}; used to update the included dictionary
+        :param vscale:       float, factor to scale the variable with
+        :returns: (norming factor, scaledUnits, scaledLatexUnits)
+
+    '''
+
+    #
+    if env=='EarthSpace':
+        scaleDict = {
+                's'      : {'defaultScale':1,
+                            1e6: {'scaledUnits':'us', 'scaledLatexUnit':r'$\mu\mathrm{s}$'},
+                            1e3: {'scaledUnits':'ms', 'scaledLatexUnit':r'$\mathrm{ms}$'}
+                        },
+                'T'      : {'defaultScale':1e9,
+                            1e9: {'scaledUnits':'nT', 'scaledLatexUnit':r'$\mathrm{nT}$'}
+                        },
+                'K'      : {'defaultScale':1e-6,
+                            1e-6:{'scaledUnits':'MK', 'scaledLatexUnit':r'$\mathrm{MK}$'}
+                        },
+                'Pa'     : {'defaultScale':1e9,
+                            1e9:{'scaledUnits':'nPa', 'scaledLatexUnit':r'$\mathrm{nPa}$'}
+                        },
+                '1/m'  :   {'defaultScale':1e3,
+                            1e3:{'scaledUnits':'1/km', 'scaledLatexUnit':r'$\mathrm{km}^{-1}$'}
+                        },
+                '1/m^3'  : {'defaultScale':1e-6,
+                            1e-6:{'scaledUnits':'1/cm^3', 'scaledLatexUnit':r'$\mathrm{cm}^{-3}$'}
+                        },
+                '1/m3'   : {'defaultScale':1e-6,
+                            1e-6:{'scaledUnits':'1/cm^3', 'scaledLatexUnit':r'$\mathrm{cm}^{-3}$'}
+                        },
+                'm/s'    : {'defaultScale':1e-3,
+                            1e-3:{'scaledUnits':'km/s', 'scaledLatexUnit':r'$\mathrm{km}\,\mathrm{s}^{-1}$'}
+                        },
+                'V/m'    : {'defaultScale':1e3,
+                            1e3:{'scaledUnits':'mV/m', 'scaledLatexUnit':r'$\mathrm{mV}\,\mathrm{m}^{-1}$'}
+                        },
+                'eV/cm3' : {'defaultScale':1e-3,
+                            1e-3:{'scaledUnits':'keV/cm^3', 'scaledLatexUnit':r'$\mathrm{keV}\,\mathrm{cm}^{-3}$'}
+                        },
+                'eV/cm^3': {'defaultScale':1e-3,
+                            1e-3:{'scaledUnits':'keV/cm^3', 'scaledLatexUnit':r'$\mathrm{keV}\,\mathrm{cm}^{-3}$'}
+                        },
+                'T/m': {'defaultScale':1e-12,
+                            1e-9:{'scaledUnits':'nT/m', 'scaledLatexUnit':r'$\mathrm{nT}\,\mathrm{m}^{-1}$'},
+                            1e-12:{'scaledUnits':'nT/km', 'scaledLatexUnit':r'$\mathrm{nT}\,\mathrm{km}^{-1}$'}
+                        },
+                'kg/m3'  : {'defaultScale':5.97863741e26,
+                            5.97863741e26:{'scaledUnits':'amu/m^3', 'scaledLatexUnit':r'$\mathrm{amu}\,\mathrm{m}^{-3}$'},
+                            5.97863741e20:{'scaledUnits':'amu/cm^3', 'scaledLatexUnit':r'$\mathrm{amu}\,\mathrm{cm}^{-3}$'}
+                        },
+                'A/m^2' :   {'defaultScale':1,
+                            1e-3:{'scaledUnits':'mA/m^2', 'scaledLatexUnit':r'$\mathrm{mA}\,\mathrm{m}^{-2}$'}
+                        },
+                'W/m^2' :   {'defaultScale':1,
+                            1e-3:{'scaledUnits':'mW/m^2', 'scaledLatexUnit':r'$\mathrm{mW}\,\mathrm{m}^{-2}$'}
+                        },
+                'm^2' :   {'defaultScale':1,
+                            1e-4:{'scaledUnits':'km^2', 'scaledLatexUnit':r'$\mathrm{km}^{2}$'}
+                        },
+                'V' :   {'defaultScale':1,
+                            1e-3:{'scaledUnits':'mV', 'scaledLatexUnit':r'$\mathrm{mV}$'}
+                        },
+                'Degrees' :   {'defaultScale':1,
+                        },
+                'mho' :   {'defaultScale':1,
+                        },
+        }
+
+    else:
+        scaleDict = {}
+    if manualDict is not None:
+        scaleDict.update(manualDict)
+    unitScale = 1.0
+    if variable_info:
+        scaledUnits = variable_info.units
+        scaledLatexUnits = variable_info.latexunits
+        units=variable_info.units
+        latexunits=variable_info.latexunits
+    else:
+        scaledUnits=''
+        scaledLatexUnits = ''
+        units=''
+        latexunits=''
+
+    if units != '':
+        dictKey = variable_info.units
+        try:
+            udict =scaleDict[dictKey]
+        except:
+            if vscale is None:
+                return 1.0, units, latexunits
+            else:
+                return vscale, units, latexunits
+        if vscale is None:
+            try:
+                unitScale = udict['defaultScale']
+            except:
+                return 1.0, units, latexunits
+        elif np.isclose(vscale, 1.0):
+            return 1.0, units, latexunits
+        else:
+            unitScale = vscale
+
+        if not any([np.isclose(unitScale, tryScale) for tryScale in udict.keys() if isinstance(tryScale, Number)]):
+            #
+            return vscale, units+" x{vscale:e}".format(vscale=vscale), latexunits+r"{\times}"+cbfmtsci(vscale,None)
+        try:
+            #above guarantees the list comprehension does not give an empty list
+            unitScale = [scale for scale in udict.keys() if isinstance(scale, Number) and np.isclose(scale,unitScale)][0]
+            scaledUnits = udict[unitScale]['scaledUnits']
+        except KeyError:
+        # logging.info('Missing scaledUnits in specialist dict for' + units + ' for unitScale='+str(unitScale))
+          return 1.0, units, latexunits
+        try:
+            scaledLatexUnits = udict[unitScale]['scaledLatexUnit']
+        except:
+        # logging.info('Missing scaledLatexUnits in specialist dict for ' + units+ ' for unitScale='+str(unitScale))
+            return 1.0, units, latexunits
+    else:
+        if vscale is None or np.isclose(vscale, 1.0):
+            return 1.0, units, latexunits
+        else:
+            return vscale, units+"x{vscale:e}".format(vscale=vscale), latexunits+r"{\times}"+cbfmtsci(vscale,None)
+
+    return unitScale, scaledUnits, scaledLatexUnits
+
+# A utility to get variableinfo with corresponding units for simple plotting. Add "canonical" scalings as
+# necessary, for default/other environments.
+def get_scaled_var(self, vscale=None, data=None, env='EarthSpace', manualDict=None,variable_info=None):
+    ''' Automatically scales the variableinfo data and adjusts the units correspondingly with the
+        default dictionaries.
+
+        :param data:         in case you wish to provide new data array (why, though?)
+        :param env:          A string to choose the scaling dictionary [default: EarthSpace]
+        :param manualDict:   a dictionary of {units : {scalingparams}}; used to update the included dictionary
+        :returns: self, with scaled units with pre-formatted units included in the varinfo.
+
+
+    '''
+
+    if variable_info is not None:
+        data=variable_info.data
+    
+    unitScale, scaledUnits, scaledLatexUnits = get_scaled_units(vscale=vscale, env=env, manualDict=manualDict)
+    if unitScale == 1: # no change, optimize out the calculation
+        return self
+
+    data = data*unitScale
+    units = scaledUnits
+    latexunits = scaledLatexUnits
+    return self
+
