67ba4074dc84bc0abf11c945663e73188964e604
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-11-25 15:27:48 +0200

    removed pointless delimiter from list

--- testpackage/testpackage_plot_vdf.py
+++ testpackage/testpackage_plot_vdf.py
@@ -121,7 +121,7 @@ restartcalls = [
 "pt.plot.plot_vdf(figsize=[5,4],vlsvobj=f, run=verifydir+REPLACEINDEX, bpara=1,coordswap=1, coordre=REPLACECOORDRE)",
 "pt.plot.plot_vdf(figsize=[5,4],vlsvobj=f, run=verifydir+REPLACEINDEX, bperp=1, coordre=REPLACECOORDRE)",
 "pt.plot.plot_vdf(figsize=[5,4],vlsvobj=f, run=verifydir+REPLACEINDEX, bperp=1,coordswap=1, coordre=REPLACECOORDRE)",
-"pt.plot.plot_vdf(figsize=[5,4],vlsvobj=f, run=verifydir+REPLACEINDEX, normal=[1,1,1],normalx=[1,-1,0],coordre=REPLACECOORDRE)",
+"pt.plot.plot_vdf(figsize=[5,4],vlsvobj=f, run=verifydir+REPLACEINDEX, normal=[1,1,1],normalx=[1,-1,0],coordre=REPLACECOORDRE)"
 ]
 nonrestartcalls = [
 # Input and output methods, nooverwrite
