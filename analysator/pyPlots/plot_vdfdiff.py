91bda6d9fe5cc74316294d8a419c3956361770b0
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-11-27 12:13:44 +0200

    minor fix to if statement and added exception to stepstr if only step is passed

--- analysator/pyPlots/plot_vdfdiff.py
+++ analysator/pyPlots/plot_vdfdiff.py
@@ -201,7 +201,7 @@ def plot_vdfdiff(filename1=None, filename2=None,
         vlsvReader2=pt.vlsvfile.VlsvReader(filename2)
     elif vlsvobj2 is not None:
         vlsvReader2=vlsvobj2
-    elif ((filedir is not None) and (step is not None)):
+    elif ((filedir is not None) and (step_diff is not None)):
         filename = glob.glob(filedir+'bulk*'+str(step_diff).rjust(7,'0')+'.vlsv')[0]
         vlsvReader2=pt.vlsvfile.VlsvReader(filename)
     else:
@@ -249,8 +249,10 @@ def plot_vdfdiff(filename1=None, filename2=None,
 
     if draw is None and axes is None:
         # step, used for file name
-        if step is not None:
+        if step is not None and step_diff is not None:
             stepstr = '_'+str(step).rjust(7,'0')+'_'+str(step_diff).rjust(7,'0')
+        elif step is not None:
+            stepstr = '_'+str(step).rjust(7,'0')
         else:
             if timeval != None:
                 stepstr = '_t'+str(int(timeval))
