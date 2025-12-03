19ec6c9e54cf628d8a1c84792552e1e5fabda27d
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-12-03 15:52:06 +0200

    fixed folder structure in run_compare

--- testpackage/run_compare.sh
+++ testpackage/run_compare.sh
@@ -16,7 +16,7 @@ do
     echo "Comparing for $i"
     #gets latest verfication set (based on modification date -> grep directories only -> take firstline -> get last word)
     folder_1="$verf_loc/$(ls -lth $verf_loc | grep ^d | head -n1 | grep -Po '\w+$')/testpackage_run/$i/" 
-    folder_2="${PWD}/produced_plots/testpackage_run/$i/"
+    folder_2="${PWD}/produced_plots/$i/"
     python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2} && echo "No differences found in produced images"
 done
 
@@ -24,6 +24,6 @@ if $check;
 then
     echo "Comparing all"
     folder_1="$verf_loc/$(ls -lth $verf_loc | grep ^d | head -n1 | grep -Po '\w+$')/testpackage_run/" 
-    folder_2="${PWD}/produced_plots/testpackage_run/"
+    folder_2="${PWD}/produced_plots/"
     python3 ../analysator/testpackage/testpackage_compare.py ${folder_1} ${folder_2} && echo "No differences found in produced images"
 fi
