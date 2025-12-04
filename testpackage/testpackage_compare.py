974da4cac18e63c7c3443e60623a960576fc71d7
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-12-04 14:06:42 +0200

    moved to using cv2 instead of image magick and added opencv-python into all in pyproject.toml

--- testpackage/testpackage_compare.py
+++ testpackage/testpackage_compare.py
@@ -3,63 +3,12 @@ import time as timetime
 import os
 import sys
 from argparse import ArgumentParser
-
-
-
-
-
-#Parse arguments
-parser = ArgumentParser(prog="Image compare"
-                        ,description="Compares images in two folders/images, and outputs the different ones to a specified output folder. Note that folders must have the same structure and filenames, otherwise these files are treated as unique."
-                        )
-
-parser.add_argument("folder_a",help="First folder/image to compare")
-parser.add_argument("folder_b",help="Second folder/image to compare")
-parser.add_argument("output_folder",help="Output folder for different images, if not specified, no output is saved",default="NULL:",nargs='?')
-
-args= parser.parse_args()
-a,b,output_folder = args.folder_a,args.folder_b,args.output_folder
-
-#Create output folder if it doesn't exist
-if not os.path.exists(output_folder) and output_folder!='NULL:':
-    proc = subprocess.Popen(f'mkdir {output_folder}'.split(" "),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    out,err = proc.communicate()
-
-    #If errors, raise an exception
-    if err:
-        err = str(err,'utf-8')
-        raise RuntimeError(err)
-
-
-def compare_images(a,b,output_file="NULL:"):
-
-    cmd = f"magick compare -metric RMSE {a} {b} {output_file}"
-    proc = subprocess.Popen(cmd.split(" "),stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-
-    out,err = proc.communicate()
-
-    exitcode=proc.returncode
-
-    #If errors, raise an exception (This has to be odne like this as compare sends output to stderr)
-    if exitcode!=0 and exitcode != 1:
-        out = str(out,'utf-8')
-        raise RuntimeError(out)
-
-    out = str(out,'utf-8') 
-    out=out.strip('\n')
-    out = out.split(" ")
-
-    #Returns true if images' RMSE = 0, i.e are identical
-    if out[0]=='0':
-        return True
-
-    return False
-
+import cv2
 
 
 #could be used to replace compare_images with a cv2 based implementation
-def compare_images_cv2(a,b):
-    import cv2
+def compare_images(a,b):
+
     im1=cv2.imread(a).astype(np.float16)
     im2=cv2.imread(b).astype(np.float16)
     #originally uint8 so we might underflow with the substraction if not casted as float, 16 should be good enough
@@ -108,11 +57,11 @@ def compare_images_in_folders(a,b,output_folder='NULL:'):
 
     #Feed the different files to compare_images
     for file in different_files:
-        if output_folder!= "NULL:":
-            filename = file.split("/")[-1].rstrip(".png") #is it always png?
-            output_folder=output_folder+f"/difference_output_{filename}.png"
+        #if output_folder!= "NULL:":
+        #    filename = file.split("/")[-1].rstrip(".png") #is it always png?
+        #    output_folder=output_folder+f"/difference_output_{filename}.png"
 
-        if(not compare_images(file,file.replace(a,b),output_folder)):
+        if(not compare_images(file,file.replace(a,b))):
             different=True
             print("Images differ:",file,file.replace(a,b))
 
@@ -136,5 +85,28 @@ def compare_images_in_folders(a,b,output_folder='NULL:'):
         print(f"::error title=Plot(s) differ::Produced plots not in agreement with the verfication set {a}")
         raise SystemError("Images Differ")
 
+if __name__=='__main__':
 
-compare_images_in_folders(a,b,output_folder)
+    #Parse arguments
+    parser = ArgumentParser(prog="Image compare"
+                            ,description="Compares images in two folders/images, and outputs the different ones to a specified output folder. Note that folders must have the same structure and filenames, otherwise these files are treated as unique."
+                            )
+
+    parser.add_argument("folder_a",help="First folder/image to compare")
+    parser.add_argument("folder_b",help="Second folder/image to compare")
+    parser.add_argument("output_folder",help="Output folder for different images, if not specified, no output is saved",default="NULL:",nargs='?')
+
+    args= parser.parse_args()
+    a,b,output_folder = args.folder_a,args.folder_b,args.output_folder
+
+    #Create output folder if it doesn't exist
+    if not os.path.exists(output_folder) and output_folder!='NULL:':
+        proc = subprocess.Popen(f'mkdir {output_folder}'.split(" "),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        out,err = proc.communicate()
+
+        #If errors, raise an exception
+        if err:
+            err = str(err,'utf-8')
+            raise RuntimeError(err)
+
+    compare_images_in_folders(a,b,output_folder)
