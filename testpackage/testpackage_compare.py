1af6e777e8a12ac938610d6e982fb84614951a7e
lassjsc <lasse.b.sihvonen@helsinki.fi>
2025-12-10 12:11:05 +0200

    found a faster way to compare the images, though no RMSE, this should produce same results as before. Left the old variant as comment incase it does not.

--- testpackage/testpackage_compare.py
+++ testpackage/testpackage_compare.py
@@ -9,33 +9,31 @@ import logging
 import os.path
 
 #could be used to replace compare_images with a cv2 based implementation
+
 def compare_images(a,b):
 
-    im1=cv2.imread(a).astype(np.float16)
-    im2=cv2.imread(b).astype(np.float16)
-    #originally uint8 so we might underflow with the substraction if not casted as float, 16 should be good enough
+    im1=cv2.imread(a)
+    im2=cv2.imread(b)
+    return im1.shape == im2.shape and not(np.bitwise_xor(im1,im2).any())
+    
+    
+    #below can be used to get the RMSE, which is about 2-3x as slow the images must be read astype(np.float16) first for accurate RMSE 
+    '''
+    #originally uint8 so we might underflow with the substraction if not casted as float, float16 should be good enough
     if im1.shape != im2.shape:
-        '''
-        something should be added to handle this better, have a threshold in general or something.
-        the substraction yields an error, so one could pad it with 
-        smaller_image=np.pad(smaller_image,(0,larger_image.shape[0]-smaller_image.shape[0]),(0,larger_image.shape[1]-smaller_image.shape[1]),(0,0))
-        '''
+
+        #something should be added to handle this better, have a threshold in general or something.
+        #the substraction yields an error, so one could pad it with 
+        #smaller_image=np.pad(smaller_image,(0,larger_image.shape[0]-smaller_image.shape[0]),(0,larger_image.shape[1]-smaller_image.shape[1]),(0,0))
+ 
         return False
     diff = np.sqrt(np.mean((im1-im2)**2))
     if diff !=0:
         return False
     return True
-
-
+    '''
 
 def compare_images_in_folders(a,b,jobcount,jobcurr):
-
-
-    #folders=get_directories(a,depth=2)
-    #folders2=get_directories(b,depth=2)
-    
-#    if len(folders)!=len(folders2):
-#        raise SystemError("Folders do not have the same structure!")
 
     #do the comparisons
     
@@ -109,35 +107,6 @@ def compare_images_in_folders(a,b,jobcount,jobcurr):
         raise SystemError("Images Differ")
 
 
-#only prints what is at the target depth (might be clunky as i was tired when writing it)
-def get_directories(directory,depth):
-    out_dirs=[]
-    dirs=[]
-    if depth==0:
-        return [directory]
-    for i in range(depth):
-
-        if i==0:
-            directory= [directory]
-            subdirs_current_depth=os.listdir(directory[0])
-            for subdir in subdirs_current_depth:
-                dirs.append(os.path.join(directory[0],subdir))
-            directory=dirs
-        else:
-            directory=[]
-
-            for dircs in dirs:
-                subdirs_current_depth=os.listdir(dircs)
-                for subdir in subdirs_current_depth:
-                    directory.append(os.path.join(dircs,subdir))
-            length_current_depth=len(directory)
-            dirs=directory
-   
-        if i==depth-1:
-            out_dirs=directory
-
-    return out_dirs
-
 
 if __name__=='__main__':
 
@@ -160,3 +129,4 @@ if __name__=='__main__':
     jobindex=args.jobindex
 
     compare_images_in_folders(a,b,jobcount,jobindex)
+    
