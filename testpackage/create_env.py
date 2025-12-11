7e619b81ec63187b8bd2c81cb9e8dbe17dfeb868
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-12-11 16:51:40 +0200

    typo

--- testpackage/create_env.py
+++ testpackage/create_env.py
@@ -24,7 +24,7 @@ def create_venv(path,install_analysator=True,editable=False):
     virt_env.post_setup(context)
     if install_analysator:
         editable='--editable' if editable else None
-        system_call(f'{path}/bin/pip install {editable} ../['all']',live_output=True)
+        system_call(f'{path}/bin/pip install {editable} ../[all]',live_output=True)
     print(f'Virtual environment created at {path}')
     return None
 
