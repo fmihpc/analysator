f3a787c9aa739f214782f9e351ab4855ea145ca4
lassejsc <lasse.b.sihvonen@helsinki.fi>
2025-12-19 15:46:56 +0200

    testing to see if removing module load works from the run_testpackage_workflow since it would make things easier for the older python

--- testpackage/run_testpackage_workflow.sh
+++ testpackage/run_testpackage_workflow.sh
@@ -19,7 +19,6 @@ hostname
 
 module purge            
 export PATH=/wrk-vakka/group/spacephysics/proj/appl/tex-basic/texlive/2023/bin/x86_64-linux:$PATH
-module load Python/3.10.4-GCCcore-11.3.0
 
 echo "SLURM_JOB_ID=$SLURM_ARRAY_JOB_ID" >> $GITHUB_OUTPUT
 
