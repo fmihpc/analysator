6d9cb051c05a65776b98ef3cf03aa48ea61f261b
lassjsc <lasse.b.sihvonen@helsinki.fi>
2026-01-08 10:25:03 +0200

    testing whether ukko nodes produce errors in workflow

--- testpackage/run_testpackage_workflow.sh
+++ testpackage/run_testpackage_workflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -t 00:45:00
 #SBATCH -J analysator_testpackage
-#SBATCH --constraint="carrington|ukko"
+#SBATCH --constraint="ukko"
 #SBATCH -p short
 #SBATCH -n 1
 #SBATCH --array=1-20
