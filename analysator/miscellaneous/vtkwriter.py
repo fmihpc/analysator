a24067e79881cb0a88ecbebecd1e7a5996aecbc9
lassjsc <lasse.b.sihvonen@helsinki.fi>
2026-01-28 11:44:36 +0200

    Comment clean up in vtkwriter.py and vlsvvtkinterface.py

--- analysator/miscellaneous/vtkwriter.py
+++ analysator/miscellaneous/vtkwriter.py
@@ -62,7 +62,7 @@ def write_vtk_file( filename, point_data ):
    
    # Add the points to the dataset
    linesPolyData.SetPoints(points)
-   
+    
    # Add the lines to the dataset
    linesPolyData.SetLines(lines)
    #######################################
@@ -76,34 +76,4 @@ def write_vtk_file( filename, point_data ):
    polyDataWriter.Write()
    #######################################
 
-#   # Read and visualize the written line
-#   #######################################
-#   reader = vtk.vtkPolyDataReader()
-#   reader.SetFileName(filename)
-#    
-#   mapper = vtk.vtkPolyDataMapper()
-#   if vtk.VTK_MAJOR_VERSION <= 5:
-#       mapper.SetInput(reader.GetOutput())
-#   else:
-#       mapper.SetInputConnection(reader.GetOutputPort())
-#    
-#   actor = vtk.vtkActor()
-#   actor.SetMapper(mapper)
-#   
-#   # Create a rendering window and renderer
-#   ren = vtk.vtkRenderer()
-#   renWin = vtk.vtkRenderWindow()
-#   renWin.AddRenderer(ren)
-#    
-#   # Create a renderwindowinteractor
-#   iren = vtk.vtkRenderWindowInteractor()
-#   iren.SetRenderWindow(renWin)
-#    
-#   # Assign actor to the renderer
-#   ren.AddActor(actor)
-#    
-#   # Enable user interface interactor
-#   iren.Initialize()
-#   renWin.Render()
-#   iren.Start()
-#   #######################################
+
