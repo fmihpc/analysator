aae5b4e0d795b954c1c3bb457d5a1915f5a6ba9f
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-02-13 17:54:47 +0200

    Does this fix CI runners import test?

--- analysator/cpphelpers/setup.py
+++ analysator/cpphelpers/setup.py
@@ -3,7 +3,6 @@ import numpy
 setup(
     name="cpphelpers",
     version="1.0",
-    description="Function to bind python and C++ together and speed pu things,requires numpy.",
     ext_modules=[Extension("cpphelpers", sources=["cpphelpers.cpp"])],
     include_dirs=[numpy.get_include()]
 )
