2bfb80d2cc7cd87cdc8d656f8a33ba7a393107d5
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-02-17 13:11:22 +0200

    Cleaned couple old comments from main __init__.py

--- analysator/__init__.py
+++ analysator/__init__.py
@@ -68,7 +68,6 @@ if os.getenv('PTNONINTERACTIVE') == None: #was ineq
          logging.info("Note: Did not import (outdated MayaVi2) grid module: " + str(e))
 
 try:
-   #import plot
    plot=lazyimport("analysator.plot")
 except ImportError as e:
    logging.info("Note: Did not import plot module: " + str(e))
@@ -79,7 +78,6 @@ def register_colormaps():
     import analysator.plot.colormaps
 
 try:
-   #import miscellaneous
    miscellaneous=lazyimport("analysator.miscellaneous")
 except ImportError as e:
    logging.info("Note: Did not import miscellaneous: " + str(e))
