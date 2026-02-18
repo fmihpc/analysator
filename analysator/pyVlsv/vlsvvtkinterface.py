5414db9da7c90d9b7e6ba22bc7a11a12399e5062
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-02-18 14:36:08 +0200

    Added the new buildDescriptor to the vlsvvtkinterface.py

--- analysator/pyVlsv/vlsvvtkinterface.py
+++ analysator/pyVlsv/vlsvvtkinterface.py
@@ -28,7 +28,7 @@ import os
 import pickle
 from operator import itemgetter
 import numbers
-
+from warnings import deprecated
 import analysator as pt
 try:
    import vtk
@@ -643,7 +643,8 @@ class VlsvVtkReader(VTKPythonAlgorithmBase):
    def GetFileName(self):
       return self.__FileName
 
-   def buildDescriptor(self):
+   @deprecated("The 'buildDescriptor' should be used instead as it is faster, this may be removed in the future but is still here in case")
+   def buildDescriptorPython(self):
       f = self.__reader
       f._VlsvReader__read_fileindex_for_cellid()
       fileindex_for_cellid = f._VlsvReader__fileindex_for_cellid
@@ -715,6 +716,20 @@ class VlsvVtkReader(VTKPythonAlgorithmBase):
             descr.write("|")
 
       return descr.getvalue(), idxToFileIndex
+    
+   def buildDescriptor(self):
+        import cpphelpers
+        f = self.__reader
+        f._VlsvReader__read_fileindex_for_cellid()
+        fileindex_for_cellid = f._VlsvReader__fileindex_for_cellid
+        xc= f._VlsvReader__xcells
+        yc= f._VlsvReader__ycells
+        zc= f._VlsvReader__zcells
+        max_ref_level = f.get_max_refinement_level()     
+        
+        ret=cpphelpers.buildDescriptor(fileindex_for_cellid,xc,yc,zc,max_ref_level)
+        #returns descriptor as first value and second is the idxToFileIndex
+        return ret[0],ret[1]
 
 
    def getDescriptor(self, reinit=False):
