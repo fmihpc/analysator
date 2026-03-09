48185b03387adf58a1dba20b884e12c3957d38b5
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-03-09 13:56:31 +0200

    Added jplots to init

--- analysator/plot/__init__.py
+++ analysator/plot/__init__.py
@@ -36,7 +36,7 @@
 #for usage with "from (package) import *"
 __all__ = ['plot_colormap', 'plot_vdf', 'plot_vdfdiff', 'plot_vdf_profiles', 'plot_colormap3dslice',
            'plot_threeslice', 'plot_ionosphere', 'plot_isosurface', 'plot_neutral_sheet',
-           'plot_variables','colormaps', 'plot_helpers']
+           'plot_variables','colormaps', 'plot_helpers','jplots']
 
 from .plot_variables import plot_variables, plot_multiple_variables
 
@@ -49,6 +49,7 @@ from . import colormaps
 
 from . import plot_helpers
 from .plot_colormap import plot_colormap
+from .plot_cutthrough_timeseries import jplots
 from .plot_vdf import plot_vdf
 from .plot_vdfdiff import plot_vdfdiff
 from .plot_vdf_profiles import plot_vdf_profiles
