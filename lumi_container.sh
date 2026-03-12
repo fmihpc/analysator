815de6301fd507539a654d97b25974484d957343
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-03-12 18:49:53 +0200

    Added way to define python version with PY_VERSION env var, defaults to 3.12 if not givne

--- lumi_container.sh
+++ lumi_container.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 #Make container.def
-if [ -f ./container.def ]; then 
+
+if [[ "$PY_VERSION" ]]; then
+  PY_PACKAGE=$(echo "$PY_VERSION" | sed 's/\.//g' )
+else
+  PY_VERSION="3.12"
+  PY_PACKAGE="312"
+fi
+
+if [ ! -f ./container.def ]; then 
   cat << EOF > container.def
-  Bootstrap: docker
-  From: docker.io/opensuse/leap:15.6
+Bootstrap: docker
+From: docker.io/opensuse/leap:15.6
 
-  %post
-    # Continue to install software into the container normally.
-    # Building:
-        # module load LUMI systools
-        # singularity build container.sif container.def
-    # Running:
-        # For example launching python (one can also launch bash for example)
-        # singularity exec container.sif python
+%post
+   # Continue to install software into the container normally.
+   # Building:
+       # module load LUMI systools
+       # singularity build container.sif container.def
+   # Running:
+       # For example launching python (one can also launch bash for example)
+       # singularity exec container.sif python
 
-        zypper -n --no-gpg-checks install python312 python312-pip python312-setuptools python312-devel git
+        zypper -n --no-gpg-checks install python$PY_PACKAGE python$PY_PACKAGE-pip python$PY_PACKAGE-setuptools python$PY_PACKAGE-devel git
 
-        update-alternatives --install /usr/bin/python python /usr/bin/python3.12 3
-        update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 3
-        update-alternatives --install /usr/bin/pip pip /usr/bin/pip3.12 3
-        update-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip3.12 3
+        update-alternatives --install /usr/bin/python python /usr/bin/python$PY_VERSION 3
+        update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$PY_VERSION 3
+        update-alternatives --install /usr/bin/pip pip /usr/bin/pip$PY_VERSION 3
+        update-alternatives --install /usr/bin/pip3 pip3 /usr/bin/pip$PY_VERSION 3
 
         export PYTHONNOUSERSITE=1
-        pip install git+https://github.com/alhom/analysator-backends/releases/tag/v0.0.2
+        pip install --extra-index-url https://version.helsinki.fi/api/v4/projects/5244/packages/pypi/simple vlsvrs
         pip install --editable git+https://github.com/fmihpc/analysator#egg=analysator"
-  EOF
+EOF
 
   #Build the container
   module load LUMI systools
   singularity build container.sif container.def
 else
   export PYTHONNOUSERSITE=1
-  singulairty exec container.def bash
+  singularity exec container.def bash
 fi
 
 
