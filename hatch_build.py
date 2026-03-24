f8324008db1a3cdf241d9c5c70569623da99d9df
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-03-24 17:35:30 +0200

    Moved the whole class inside the if statement

--- hatch_build.py
+++ hatch_build.py
@@ -4,18 +4,18 @@ from packaging.version import Version
 # import logging
 # logger = logging.getLogger(__name__)
 import platform
-if Version(platform.python_version()) < Version("3.8"):
-    quit()
-class CustomBuildHook(BuildHookInterface):
-    def initialize(self,*param,**kwargs):
-        with open("./analysator/miscellaneous/_commithash.py","w") as file:
-            commithash=subprocess.run(["git","rev-parse","HEAD"],capture_output=True).stdout.decode('utf-8')
-            commithash=commithash.strip("\n")
-            file.write(f"commithash='{commithash}'"+"\n")
-            file.close()
-       
-    def finalize(self, *param,**kwargs)->None:
-        out=subprocess.run([sys.executable, "-m", "pip", "install", "-r","requirements-backend.txt","-v"],capture_output=True,check=True)
-        #logger.error("TESTINGTESTING ALERTA"+out.stdout.decode('utf-8')) #Doesnt work
-        #print(out) #Also doesnt work
-        # self.app.display_error(out.stdout.decode('utf-8')) #Does not work
+if not Version(platform.python_version()) < Version("3.8"):
+        
+    class CustomBuildHook(BuildHookInterface):
+        def initialize(self,*param,**kwargs):
+            with open("./analysator/miscellaneous/_commithash.py","w") as file:
+                commithash=subprocess.run(["git","rev-parse","HEAD"],capture_output=True).stdout.decode('utf-8')
+                commithash=commithash.strip("\n")
+                file.write(f"commithash='{commithash}'"+"\n")
+                file.close()
+           
+        def finalize(self, *param,**kwargs)->None:
+            out=subprocess.run([sys.executable, "-m", "pip", "install", "-r","requirements-backend.txt","-v"],capture_output=True,check=True)
+            #logger.error("TESTINGTESTING ALERTA"+out.stdout.decode('utf-8')) #Doesnt work
+            #print(out) #Also doesnt work
+            # self.app.display_error(out.stdout.decode('utf-8')) #Does not work
