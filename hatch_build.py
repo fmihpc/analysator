c74f3a20d245d68b4732e0c19921b4eada481035
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-03-24 17:16:30 +0200

    Added automatic backend install hook and writing of the commit has into a python file that can be imported in analysator

--- hatch_build.py
+++ hatch_build.py
@@ -1,6 +1,16 @@
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 import sys, subprocess
+# import logging
+# logger = logging.getLogger(__name__)
 class CustomBuildHook(BuildHookInterface):
-    
-    def finalize(self, version: str, build_data: dict[str, Any], artifact_path: str) -> None:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r","requirements-backend.txt"])
+    def initialize(self,*param,**kwargs):
+        with open("./analysator/miscellaneous/_commithash.py","w") as file:
+            commithash=subprocess.run(["git","rev-parse","HEAD"],capture_output=True).stdout.decode('utf-8')
+            file.write(f"commithash='{commithash.strip('\n')}'\n")
+            file.close()
+       
+    def finalize(self, version: str, build_data: dict[str, Any], artifact_path: str)->None:
+        out=subprocess.run([sys.executable, "-m", "pip", "install", "-r","requirements-backend.txt","-v"],capture_output=True,check=True)
+        #logger.error("TESTINGTESTING ALERTA"+out.stdout.decode('utf-8')) #Doesnt work
+        #print(out) #Also doesnt work
+        # self.app.display_error(out.stdout.decode('utf-8')) #Does not work
