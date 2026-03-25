ca416167f252d4d6770a8c09edb4628f01d5124e
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-03-25 15:12:08 +0200

    Removed the automatic installation of backends with pip --install due to it not being possible to take into account the optional vlsvrs_build dependency.

--- hatch_build.py
+++ hatch_build.py
@@ -1,24 +1,12 @@
 import sys, subprocess
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from packaging.version import Version
-# import logging
-# logger = logging.getLogger(__name__)
-import platform
-if not Version(platform.python_version()) < Version("3.8"):
-        
-    class CustomBuildHook(BuildHookInterface):
-        def initialize(self,*param,**kwargs):
-            with open("./analysator/miscellaneous/_commithash.py","w") as file:
-                commithash=subprocess.run(["git","rev-parse","HEAD"],capture_output=True).stdout.decode('utf-8')
-                commithash=commithash.strip("\n")
-                file.write(f"commithash='{commithash}'"+"\n")
-                file.close()
-           
-        def finalize(self, *param,**kwargs)->None:
-            out=subprocess.run([sys.executable, "-m", "pip", "install", "-r","requirements-backend.txt","-v"],capture_output=True,check=True)
-            #logger.error("TESTINGTESTING ALERTA"+out.stdout.decode('utf-8')) #Doesnt work
-            #print(out) #Also doesnt work
-            # self.app.display_error(out.stdout.decode('utf-8')) #Does not work
-else:
-    class CustomBuildHook(BuildHookInterface):
-        print("eh")
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self,*param,**kwargs):
+        with open("./analysator/miscellaneous/_commithash.py","w") as file:
+            commithash=subprocess.run(["git","rev-parse","HEAD"],capture_output=True).stdout.decode('utf-8')
+            commithash=commithash.strip("\n")
+            file.write(f"commithash='{commithash}'"+"\n")
+            file.close()
+
